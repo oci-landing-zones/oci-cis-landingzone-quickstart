bfd138194bc0f42d9d263018837947694c425ce9
Andre Correa <andre.correa@oracle.com>
2020-10-27 11:53:58 -0300

    .gitignore updated

--- cis-monitoring/cis3_1.tf
+++
@@ -1,4 +0,0 @@
-resource "oci_audit_configuration" "this" {
-    compartment_id = var.tenancy_ocid
-    retention_period_days = 365
-}    
--- modules/network/security/common_locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  anywhere          = "0.0.0.0/0"
-  ssh_port          = "22"
-  rdp_port          = "3389"
-}

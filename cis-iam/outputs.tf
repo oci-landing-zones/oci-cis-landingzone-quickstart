857f6ef05240bed154437f1beeef3801122a09b7
Andre Correa <andre.correa@oracle.com>
2020-11-06 09:39:13 -0300

    Hiding outputs, except for the reporting use cases in the CIS document

--- cis-iam/outputs.tf
+++ cis-iam/outputs.tf
@@ -1,27 +1,32 @@
 output "offending_policies" {
-  value = local.offending_policies
+  value = length(local.offending_policies) > 0 ? local.offending_policies : null
 }
 
 output "to_do" {
-  value = length(local.offending_policies) > 0 ? "Please remove the statement(s) in offending_policies. The grant 'manage all-resources in tenancy' should be given to the Administrators group only." : "No offending policies found. Nothing to do."
+  value = length(local.offending_policies) > 0 ? "Please remove the statement(s) shown in offending_policies output. The grant 'manage all-resources in tenancy' should be given to the Administrators group only." : null
 }
 
 output "network_compartment_id" {
   value = module.compartments.compartments[local.network_compartment_name].id
+  sensitive = true
 }
 
 output "security_compartment_id" {
   value = module.compartments.compartments[local.security_compartment_name].id
+  sensitive = true
 }
 
 output "compute_storage_compartment_id" {
   value = module.compartments.compartments[local.compute_storage_compartment_name].id
+  sensitive = true
 }
 
 output "appdev_compartment_id" {
   value = module.compartments.compartments[local.appdev_compartment_name].id
+  sensitive = true
 }
 
 output "database_compartment_id" {
   value = module.compartments.compartments[local.database_compartment_name].id
+  sensitive = true
 }

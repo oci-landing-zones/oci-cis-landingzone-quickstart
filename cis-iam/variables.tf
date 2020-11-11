34ee3126acf57d878e80a9038367c9c52727d80d
Andre Correa <andre.correa@oracle.com>
2020-11-11 15:49:20 -0300

    Old config folders delete

--- cis-iam/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "tenancy_ocid" {}
-variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
-variable "private_key_password" {}
-variable "home_region" {}
-variable "service_label" {}

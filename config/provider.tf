64baa349e24faddd2796131f562d17b4a553c8e8
Andre Correa <andre.correa@oracle.com>
2020-11-12 14:33:44 -0300

    OCI provider version added

--- config/provider.tf
+++ config/provider.tf
@@ -2,10 +2,11 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 provider "oci" {
-  region           = var.home_region
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
+  region               = var.home_region
+  version              = "~> 4.2"
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
   private_key_password = var.private_key_password
 }

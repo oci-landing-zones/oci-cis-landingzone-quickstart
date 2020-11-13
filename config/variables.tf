8747d82a23399e75f960c9a11be1bace0109e2c3
Andre Correa <andre.correa@oracle.com>
2020-11-13 10:23:27 -0300

    Added validation rules to variables.

--- config/variables.tf
+++ config/variables.tf
@@ -27,40 +27,81 @@ variable "region_key" {
 }
 variable "service_label" {
     default = "cis"
+    validation {
+        condition     = length(regexall("_",var.service_label)) == 0
+        error_message = "The service_label variable value must not contain the character _."
+    }
 }
 
 # Networking
 variable "vcn_cidr" {
     default = "10.0.0.0/16"
+    validation { 
+        condition = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))?$",var.vcn_cidr)) > 0
+        error_message = "Invalid cidr block value provided for vcn_cidr variable."
+    }
 }
 variable "public_subnet_cidr" {
     default = "10.0.1.0/24"
+    validation { 
+        condition = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))?$",var.public_subnet_cidr)) > 0
+        error_message = "Invalid cidr block value provided for public_subnet_cidr variable."
+    }
 }
 variable "private_subnet_app_cidr" {
     default = "10.0.2.0/24"
+    validation { 
+        condition = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))?$",var.private_subnet_app_cidr)) > 0
+        error_message = "Invalid cidr block value provided for private_subnet_app_cidr variable."
+    }
 }
 variable "private_subnet_db_cidr" {
     default = "10.0.3.0/24"
+    validation { 
+        condition = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))?$",var.private_subnet_db_cidr)) > 0
+        error_message = "Invalid cidr block value provided for private_subnet_db_cidr variable."
+    }
 }
 variable "public_src_bastion_cidr" {
-    default = "177.235.205.77/32"
+    validation {
+        condition     = var.public_src_bastion_cidr != "0.0.0.0/0"
+        error_message = "The public_src_bastion_cidr variable value must be different than 0.0.0.0/0."
+    }
 }
 variable "public_src_lbr_cidr" {
     default = "0.0.0.0/0"
+    validation { 
+        condition = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))?$",var.public_src_lbr_cidr)) > 0
+        error_message = "Invalid cidr block value provided for public_src_lbr_cidr variable."
+    }
 }
 
 # Monitoring
 variable "network_admin_email_endpoint" {
-    default = "andre.correa@oracle.com"
+    validation { 
+        condition = length(regexall("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",var.network_admin_email_endpoint)) > 0
+        error_message = "Invalid email address value provided for network_admin_email_endpoint variable."
+    }
 }
 variable "security_admin_email_endpoint" {
-    default = "andremo_br@yahoo.com"
+    validation { 
+        condition = length(regexall("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",var.security_admin_email_endpoint)) > 0
+        error_message = "Invalid email address value provided for security_admin_email_endpoint variable."
+    }
 }
 variable "cloud_guard_configuration_status" {
   default = "ENABLED"
+  validation {
+      condition = var.cloud_guard_configuration_status == "ENABLED" || var.cloud_guard_configuration_status == "DISABLED"
+      error_message = "Invalid value provided for cloud_guard_configuration_status. Valid values: ENABLED or DISABLED."
+  }
 }
 # Setting this variable to true lets the user seed the oracle managed entities with minimal changes to the original entities.
 # False will delegate this responsibility to CloudGuard for seeding the oracle managed entities.
 variable "cloud_guard_configuration_self_manage_resources" {
-  default = false
+    default = false
+    validation {
+      condition = can(tobool(var.cloud_guard_configuration_self_manage_resources))
+      error_message = "Invalid value provided for cloud_guard_configuration_self_manage_resources. Valid values: true or false."
+  }
 }

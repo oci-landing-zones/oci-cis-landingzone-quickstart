64b71cfbb8a3be2bc9b448e288b4419a552eec32
Andre Correa <andre.correa@oracle.com>
2020-11-18 10:35:46 -0300

    Updated formatting

--- modules/object-storage/bucket/outputs.tf
+++ modules/object-storage/bucket/outputs.tf
@@ -1,11 +1,8 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-# Output Buckets indexed by bucket name
+# Buckets indexed by bucket name
 output "oci_objectstorage_buckets" {
   description = "The buckets, indexed by bucket name."
-  value = {
-    for bkt in oci_objectstorage_bucket.these:
-      bkt.name => bkt
-}
+  value       = {for bkt in oci_objectstorage_bucket.these: bkt.name => bkt}
 }

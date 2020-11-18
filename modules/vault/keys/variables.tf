1bff886cf16c02d400055fb631d753dbc098d9c4
Andre Correa <andre.correa@oracle.com>
2020-11-18 12:26:10 -0300

    Unused variables removed.

--- modules/vault/keys/variables.tf
+++ modules/vault/keys/variables.tf
@@ -1,21 +1,9 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-variable "tenancy_ocid" {
-  type        = string
-  description = "Tenancy ID"
-  default     = ""
-} 
-
 variable "compartment_id" {
   type        = string
   description = "The default compartment OCID to use for resources (unless otherwise specified)."
-  default     = ""
-} 
-
-variable "compartment_name" {
-  type        = string
-  description = "Compartment anme"
   default     = ""
 } 
 
@@ -29,12 +17,6 @@ variable "vault_type" {
   type        = string
   description = "Vault Type - DEFAULT (Shared)"
   default     = "DEFAULT"
-}  
-
-variable "region" {
-  type        = string
-  description = "Region"
-  default     = ""
 }  
 
 variable "keys" {

ef69839d61cd37e0eb33a716c5c8961e0ba7db75
Andre Correa <andre.correa@oracle.com>
2020-11-23 11:59:24 -0300

    Merged Compute_Storage compartment into AppDev.

--- config/locals.tf
+++ config/locals.tf
@@ -5,19 +5,16 @@ locals {
     ### IAM
     security_compartment_name        = "${var.service_label}-Security"
     network_compartment_name         = "${var.service_label}-Network"
-    compute_storage_compartment_name = "${var.service_label}-ComputeStorage"
     database_compartment_name        = "${var.service_label}-Database"
     appdev_compartment_name          = "${var.service_label}-AppDev" 
 
     security_compartment_name_output        = module.cis_compartments.compartments[local.security_compartment_name].name
     network_compartment_name_output         = module.cis_compartments.compartments[local.network_compartment_name].name
-    compute_storage_compartment_name_output = module.cis_compartments.compartments[local.compute_storage_compartment_name].name
     database_compartment_name_output        = module.cis_compartments.compartments[local.database_compartment_name].name
     appdev_compartment_name_output          = module.cis_compartments.compartments[local.appdev_compartment_name].name
 
     security_admin_group_name        = "${var.service_label}-SecurityAdmins"
     network_admin_group_name         = "${var.service_label}-NetworkAdmins"
-    compute_storage_admin_group_name = "${var.service_label}-ComputeStorageAdmins"
     database_admin_group_name        = "${var.service_label}-DatabaseAdmins"
     appdev_admin_group_name          = "${var.service_label}-AppDevAdmins"
     iam_admin_group_name             = "${var.service_label}-IAMAdmins"

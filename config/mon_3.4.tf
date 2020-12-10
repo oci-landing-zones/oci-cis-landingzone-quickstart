c4c4085b53f4d5532657f4d3d25bddc49175d575
Andre Correa <andre.correa@oracle.com>
2020-12-10 08:22:52 -0300

    Removed commented code.

--- config/mon_3.4.tf
+++ config/mon_3.4.tf
@@ -7,7 +7,6 @@ module "cis_notification_idp_changes" {
   rule_display_name  = "${var.service_label}-notify-on-idp-changes"    
   rule_description   = "Sends notification when Identity Providers are created, updated or deleted."
   rule_is_enabled    = true
-  #rule_condition     = "{\"eventType\": [\"com.oraclecloud.identityControlPlane.CreateIdentityProvider\",\"com.oraclecloud.identityControlPlane.DeleteIdentityProvider\",\"com.oraclecloud.identityControlPlane.UpdateIdentityProvider\"]}"
   rule_condition     = <<EOT
   {"eventType": 
     ["com.oraclecloud.identityControlPlane.CreateIdentityProvider",

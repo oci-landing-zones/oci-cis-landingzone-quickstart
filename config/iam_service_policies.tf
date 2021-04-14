5f05c36a295e9e35721309828381e843215d0d51
Andre Correa <andre.correa@oracle.com>
2021-04-14 18:03:06 -0300

    Policies for Scanning service

--- config/iam_service_policies.tf
+++ config/iam_service_policies.tf
@@ -1,11 +1,11 @@
-module "service_policies" {
+module "cis_service_policies" {
     source = "../modules/iam/iam-policy"
     providers = { oci = oci.home }
     policies = {
         ("${var.service_label}-OS-ManagementAcess-Policy") = {
             compartment_id = var.tenancy_ocid
             description    = "Policy to allow the OS Management service permission to read instance information in the tenancy."
-            statements     = ["Allow service osms to read instances in tenancy"]
+            statements     = ["allow service osms to read instances in tenancy"]
         },
         ("${var.service_label}-Cloud-GuardAccess-Policy") = {
             compartment_id = var.tenancy_ocid
@@ -27,6 +27,14 @@ module "service_policies" {
                             "allow service cloudguard to read dynamic-groups in tenancy",
                             "allow service cloudguard to read authentication-policies in tenancy",
                             "allow service cloudguard to use network-security-groups in tenancy"]
+        },
+        ("${var.service_label}-Vulnerability-Scanning-Policy") = {
+            compartment_id = var.tenancy_ocid
+            description    = "Policy to allow the Vulnerability Scanning service permission on compute instances in the tenancy."
+            statements     = ["allow service vulnerability-scanning-service to manage instances in tenancy",
+                               "allow service vulnerability-scanning-service to read compartments in tenancy",
+                               "allow service vulnerability-scanning-service to read vnics in tenancy",
+                               "allow service vulnerability-scanning-service to read vnic-attachments in tenancy"]
         }
     }
 }

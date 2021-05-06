98de08603d44ea9de813258128b33fab150c3a5b
Andre Correa <andre.correa@oracle.com>
2021-05-06 15:03:00 -0300

    policy changes

--- pre-config/locals.tf
+++ pre-config/locals.tf
@@ -1,9 +1,9 @@
 locals {
-  top_compartment_name       = var.lz_top_compartment_name != null ? var.lz_top_compartment_name : "${var.unique_prefix}-top-cmp"
-  top_compartment_parent_id  = var.lz_top_compartment_parent_id != null ? var.lz_top_compartment_parent_id : var.tenancy_ocid
-  provisioning_group_name    = var.lz_provisioning_group_name != null ? var.lz_provisioning_group_name : "${var.unique_prefix}-provisioning-group"
-  provisioning_policy_name   = "${var.unique_prefix}-provisioning-policy"
-  security_admin_policy_name = "${var.unique_prefix}-security-admin-group-policy"
+  top_compartment_name      = var.lz_top_compartment_name != null ? var.lz_top_compartment_name : "${var.unique_prefix}-top-cmp"
+  top_compartment_parent_id = var.lz_top_compartment_parent_id != null ? var.lz_top_compartment_parent_id : var.tenancy_ocid
+  provisioning_group_name   = var.lz_provisioning_group_name != null ? var.lz_provisioning_group_name : "${var.unique_prefix}-provisioning-group"
+  provisioning_policy_name  = "${var.unique_prefix}-provisioning-policy"
+  tenancy_level_policy_name = "${var.unique_prefix}-groups-tenancy-level-policy"
 
   iam_admin_group_name           = "${var.unique_prefix}-iam-admin-group"
   network_admin_group_name       = "${var.unique_prefix}-network-admin-group"

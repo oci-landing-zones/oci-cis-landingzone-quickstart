36593e416eb3637c7497cf49763dc1c60b6cac1b
Josh Hammer <josh@halimer.com>
2021-05-10 18:14:06 -0400

    Added Target Detector Recipes for activity and configuration

--- modules/monitoring/cloud-guard/main.tf
+++ modules/monitoring/cloud-guard/main.tf
@@ -2,6 +2,13 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 # CloudGuard enabling and disabling is a tenant-level operation 
+
+data "oci_cloud_guard_detector_recipes" "compartment_detector_recipes" {
+    depends_on = [ oci_cloud_guard_cloud_guard_configuration.this]
+      #Required
+      compartment_id = "ocid1.tenancy.oc1..aaaaaaaabkqehju37orqb2rs6qqtte4p4elzyfjtjkjdq5bkihntaegh2taa"
+  }
+
 resource "oci_cloud_guard_cloud_guard_configuration" "this" {
   #Required
   compartment_id        = var.compartment_id
@@ -11,8 +18,16 @@ resource "oci_cloud_guard_cloud_guard_configuration" "this" {
 }
 
 resource "oci_cloud_guard_target" "this" {
+  depends_on = [ oci_cloud_guard_cloud_guard_configuration.this]
   compartment_id       = var.compartment_id
   display_name         = var.default_target.name
   target_resource_id   = var.default_target.id
   target_resource_type = var.default_target.type
+    target_detector_recipes {
+        detector_recipe_id = data.oci_cloud_guard_detector_recipes.compartment_detector_recipes.detector_recipe_collection[0].items[0].id
+    }
+    target_detector_recipes {
+        detector_recipe_id = data.oci_cloud_guard_detector_recipes.compartment_detector_recipes.detector_recipe_collection[0].items[1].id
+    }
 }
+

b31f10270ba759ed0df139db4d6dd983586ee388
Andre Correa <andre.correa@oracle.com>
2021-05-10 12:37:02 -0300

    Creating default CG target for root comparment

--- modules/monitoring/cloud-guard/main.tf
+++ modules/monitoring/cloud-guard/main.tf
@@ -4,10 +4,15 @@
 # CloudGuard enabling and disabling is a tenant-level operation 
 resource "oci_cloud_guard_cloud_guard_configuration" "this" {
   #Required
-  compartment_id   = var.compartment_id
-  reporting_region = var.reporting_region
-  status           = var.status
-
-  #Optional
+  compartment_id        = var.compartment_id
+  reporting_region      = var.reporting_region
+  status                = var.status
   self_manage_resources = var.self_manage_resources
+}
+
+resource "oci_cloud_guard_target" "this" {
+  compartment_id       = var.compartment_id
+  display_name         = var.default_target_name
+  target_resource_id   = var.compartment_id
+  target_resource_type = "COMPARTMENT"
 }

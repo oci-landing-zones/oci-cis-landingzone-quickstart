56943095cc8c6b06c136db33a2ae4de88001eb09
Andre Correa <andre.correa@oracle.com>
2021-05-12 11:33:55 -0300

    Split module, keeping CG policies at the tenancy level always

--- config/iam_service_policies.tf
+++ config/iam_service_policies.tf
@@ -1,32 +1,40 @@
-module "service_policies" {
+module "lz_os_management_policies" {
     source = "../modules/iam/iam-policy"
     providers = { oci = oci.home }
     policies = {
-        ("${var.service_label}-OS-ManagementAcess-Policy") = {
+        (local.os_mgmt_policy_name) = {
             compartment_id = local.parent_compartment_id
             description    = "Policy to allow the OS Management service permission to read instance information in ${local.policy_level}."
             statements     = ["Allow service osms to read instances in ${local.policy_level}"]
-        },
-        ("${var.service_label}-Cloud-GuardAccess-Policy") = {
-            compartment_id = local.parent_compartment_id
-            description    = "Policy for Cloud Guard to review ${local.policy_level}"
-            statements     = ["allow service cloudguard to read keys in ${local.policy_level}",
-                            "allow service cloudguard to read compartments in ${local.policy_level}",
-                            "allow service cloudguard to read tenancies in ${local.policy_level}",
-                            "allow service cloudguard to read audit-events in ${local.policy_level}",
-                            "allow service cloudguard to read compute-management-family in ${local.policy_level}",
-                            "allow service cloudguard to read instance-family in ${local.policy_level}",
-                            "allow service cloudguard to read virtual-network-family in ${local.policy_level}",
-                            "allow service cloudguard to read volume-family in ${local.policy_level}",
-                            "allow service cloudguard to read database-family in ${local.policy_level}",
-                            "allow service cloudguard to read object-family in ${local.policy_level}",
-                            "allow service cloudguard to read load-balancers in ${local.policy_level}",
-                            "allow service cloudguard to read users in ${local.policy_level}",
-                            "allow service cloudguard to read groups in ${local.policy_level}",
-                            "allow service cloudguard to read policies in ${local.policy_level}",
-                            "allow service cloudguard to read dynamic-groups in ${local.policy_level}",
-                            "allow service cloudguard to read authentication-policies in ${local.policy_level}",
-                            "allow service cloudguard to use network-security-groups in ${local.policy_level}"]
+        }
+    }
+}
+
+module "lz_cloud_guard_policies" {
+    count = var.use_existing_tenancy_policies == false ? 1 : 0
+    source = "../modules/iam/iam-policy"
+    providers = { oci = oci.home }
+    policies = {
+        (local.cloud_guard_policy_name) = {
+            compartment_id = var.tenancy_ocid
+            description    = "Policy for Cloud Guard to review a tenancy."
+            statements     = ["Allow service cloudguard to read keys in tenancy",
+                            "Allow service cloudguard to read compartments in tenancy",
+                            "Allow service cloudguard to read tenancies in tenancy",
+                            "Allow service cloudguard to read audit-events in tenancy",
+                            "Allow service cloudguard to read compute-management-family in tenancy",
+                            "Allow service cloudguard to read instance-family in tenancy",
+                            "Allow service cloudguard to read virtual-network-family in tenancy",
+                            "Allow service cloudguard to read volume-family in tenancy",
+                            "Allow service cloudguard to read database-family in tenancy",
+                            "Allow service cloudguard to read object-family in tenancy",
+                            "Allow service cloudguard to read load-balancers in tenancy",
+                            "Allow service cloudguard to read users in tenancy",
+                            "Allow service cloudguard to read groups in tenancy",
+                            "Allow service cloudguard to read policies in tenancy",
+                            "Allow service cloudguard to read dynamic-groups in tenancy",
+                            "Allow service cloudguard to read authentication-policies in tenancy",
+                            "Allow service cloudguard to use network-security-groups in tenancy"]
         }
     }
 }

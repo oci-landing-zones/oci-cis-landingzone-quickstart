aff33e04079782e22c74e1d51e60ccb0eb1c9447
Andre Correa <andre.correa@oracle.com>
2021-05-19 11:40:57 -0300

    group output added

--- modules/iam/iam-group/outputs.tf
+++ modules/iam/iam-group/outputs.tf
@@ -1,6 +1,10 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+output "group" {
+  value = oci_identity_group.this
+}
+  
 output "group_id" {
   value = oci_identity_group.this.id
 }

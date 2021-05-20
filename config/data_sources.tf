b1fe6b14c3c622f18b8256973854e98d00015429
Andre Correa <andre.correa@oracle.com>
2021-05-20 17:31:48 -0300

    checking existing groups in data source

--- config/data_sources.tf
+++ config/data_sources.tf
@@ -1,6 +1,76 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+data "oci_identity_regions" "these" {}
+
+data "oci_identity_tenancy" "this" {
+  tenancy_id = var.tenancy_ocid
+}
+
 data "oci_identity_compartment" "existing_enclosing_compartment" {
     id = var.existing_enclosing_compartment_ocid
+}
+
+data "oci_identity_groups" "existing_iam_admin_group" {
+    compartment_id = var.tenancy_ocid
+    filter {
+        name = "name"
+        values = [var.existing_iam_admin_group_name,var.existing_cred_admin_group_name]
+    }
+}
+
+data "oci_identity_groups" "existing_cred_admin_group" {
+    compartment_id = var.tenancy_ocid
+    filter {
+        name = "name"
+        values = [var.existing_cred_admin_group_name]
+    }
+}
+
+data "oci_identity_groups" "existing_security_admin_group" {
+    compartment_id = var.tenancy_ocid
+    filter {
+        name = "name"
+        values = [var.existing_security_admin_group_name]
+    }
+}
+
+data "oci_identity_groups" "existing_network_admin_group" {
+    compartment_id = var.tenancy_ocid
+    filter {
+        name = "name"
+        values = [var.existing_network_admin_group_name]
+    }
+}
+
+data "oci_identity_groups" "existing_appdev_admin_group" {
+    compartment_id = var.tenancy_ocid
+    filter {
+        name = "name"
+        values = [var.existing_appdev_admin_group_name]
+    }
+}
+
+data "oci_identity_groups" "existing_database_admin_group" {
+    compartment_id = var.tenancy_ocid
+    filter {
+        name = "name"
+        values = [var.existing_database_admin_group_name]
+    }
+}
+
+data "oci_identity_groups" "existing_auditor_group" {
+    compartment_id = var.tenancy_ocid
+    filter {
+        name = "name"
+        values = [var.existing_auditor_group_name]
+    }
+}
+
+data "oci_identity_groups" "existing_announcement_reader_group" {
+    compartment_id = var.tenancy_ocid
+    filter {
+        name = "name"
+        values = [var.existing_announcement_reader_group_name]
+    }
 }

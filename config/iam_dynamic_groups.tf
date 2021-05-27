30ee08a39d80ba27be94fbb4c0ee6fd1701ecc55
Andre Correa <andre.correa@oracle.com>
2021-05-27 11:47:06 -0300

    Added copyright comments

--- config/iam_dynamic_groups.tf
+++ config/iam_dynamic_groups.tf
@@ -1,3 +1,6 @@
+# Copyright (c) 2020 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 module "cis_dynamic_groups" {
     depends_on = [module.cis_compartments]
     source = "../modules/iam/iam-dynamic-group"

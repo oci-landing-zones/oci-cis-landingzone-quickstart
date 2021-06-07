101682b2ae86667eda76c127fa0807c73c16b579
Andre Correa <andre.correa@oracle.com>
2021-06-07 15:20:48 -0300

    Lower cased target description

--- config/vss.tf
+++ config/vss.tf
@@ -33,28 +33,28 @@ module "cis_scanning" {
     scan_targets = var.vss_create == true ? {
         (local.security_cmp_target_name) = {
             compartment_id = module.cis_compartments.compartments[local.security_compartment_name].id 
-            description = "${local.security_compartment_name} compartment Scanning target."
+            description = "${local.security_compartment_name} compartment scanning target."
             scan_recipe_name = local.scan_default_recipe_name
             target_compartment_id = module.cis_compartments.compartments[local.security_compartment_name].id
             defined_tags = null
         },
         (local.network_cmp_target_name) = {
             compartment_id = module.cis_compartments.compartments[local.security_compartment_name].id 
-            description = "${local.network_compartment_name} compartment Scanning target."
+            description = "${local.network_compartment_name} compartment scanning target."
             scan_recipe_name = local.scan_default_recipe_name
             target_compartment_id = module.cis_compartments.compartments[local.network_compartment_name].id
             defined_tags = null
         },
         (local.appdev_cmp_target_name) = {
             compartment_id = module.cis_compartments.compartments[local.security_compartment_name].id 
-            description = "${local.appdev_compartment_name} compartment Scanning target."
+            description = "${local.appdev_compartment_name} compartment scanning target."
             scan_recipe_name = local.scan_default_recipe_name
             target_compartment_id = module.cis_compartments.compartments[local.appdev_compartment_name].id
             defined_tags = null
         },
         (local.database_cmp_target_name) = {
             compartment_id = module.cis_compartments.compartments[local.security_compartment_name].id 
-            description = "${local.database_compartment_name} compartment Scanning target."
+            description = "${local.database_compartment_name} compartment scanning target."
             scan_recipe_name = local.scan_default_recipe_name
             target_compartment_id = module.cis_compartments.compartments[local.database_compartment_name].id
             defined_tags = null

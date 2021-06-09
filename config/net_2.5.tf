9a9536583d49584971f9b5609ebe34d512b891d2
Josh Hammer <josh@halimer.com>
2021-06-09 18:58:10 -0400

    Updated Hub VCN to DMZ VCN

--- config/net_2.5.tf
+++ config/net_2.5.tf
@@ -25,7 +25,7 @@ resource "oci_core_default_security_list" "default_security_list" {
   }
 }
 
-resource "oci_core_default_security_list" "hub_default_security_list" {
+resource "oci_core_default_security_list" "dmz_default_security_list" {
   count                      = var.hub_spoke_architecture == true ? 1 : 0
   manage_default_resource_id = module.cis_dmz_vcn[0].vcn.default_security_list_id
   ingress_security_rules {
@@ -40,7 +40,7 @@ resource "oci_core_default_security_list" "hub_default_security_list" {
   ingress_security_rules {
     protocol  = "1"
     stateless = false
-    source    = var.hub_vcn_cidr
+    source    = var.dmz_vcn_cidr
     icmp_options {
       type = 3
       code = null

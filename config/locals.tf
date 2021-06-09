e50bc4ea4ef69faae92a620185349b2d33eda5a9
Josh Hammer <josh@halimer.com>
2021-06-08 21:09:29 -0400

    Updated locals and variables to support Hub & Spoke

--- config/locals.tf
+++ config/locals.tf
@@ -29,32 +29,78 @@ locals {
 
     ### Network
     anywhere = "0.0.0.0/0"
-    valid_service_gateway_cidrs = ["oci-${local.region_key}-objectstorage", "all-${local.region_key}-services-in-oracle-services-network"]
+    valid_service_gateway_cidrs = [ "all-${local.region_key}-services-in-oracle-services-network", "oci-${local.region_key}-objectstorage"]
 
-    # VCN names
+    ## DMZ VCN
+    # DMZ VCN Name
+    dmz_vcn_display_name = "${var.service_label}-dmz-vcn"
+
+    # DMZ Subnet names
+    dmz_bastion_subnet_name = "${var.service_label}-bastion-subnet"
+    dmz_services_subnet_name   = "${var.service_label}-services-subnet"
+
+
+    # DMZ Security lists names
+    dmz_bastion_subnet_security_list_name = "${local.dmz_bastion_subnet_name}-security-list"
+    dmz_services_subnet_security_list_name = "${local.dmz_services_subnet_name}-security-list"
+
+    # DMZ Route tables names
+    dmz_bastion_subnet_route_table_name = "${local.dmz_bastion_subnet_name}-route-table"
+    dmz_services_subnet_route_table_name   = "${local.dmz_services_subnet_name}-route-table"
+
+    # DMZ Network security groups names
+    dmz_bastion_nsg_name   = "${var.service_label}-dmz-nsg-Bastion"
+    dmz_services_nsg_name   = "${var.service_label}-Hub-NSG-Services"
+
+    # Spoke 1 or Single VCN names
     vcn_display_name = "${var.service_label}-VCN"
   
-    # Subnet names
+    # Spoke 1 or Single VCN Subnet names
     public_subnet_name      = "${var.service_label}-Public-Subnet"
     private_subnet_app_name = "${var.service_label}-Private-Subnet-App"
     private_subnet_db_name  = "${var.service_label}-Private-Subnet-DB"
     
-    # Security lists names
+    # Spoke 1 or Single VCN Security lists names
     public_subnet_security_list_name      = "${local.public_subnet_name}-Security-List"
     private_subnet_app_security_list_name = "${local.private_subnet_app_name}-Security-List"
     private_subnet_db_security_list_name  = "${local.private_subnet_db_name}-Security-List"
     
-    # Network security groups names
+    # Spoke 1 or Single VCN Network security groups names
     bastion_nsg_name = "${var.service_label}-NSG-Bastion"
     lbr_nsg_name     = "${var.service_label}-NSG-LBR"
     app_nsg_name     = "${var.service_label}-NSG-App"
     db_nsg_name      = "${var.service_label}-NSG-DB"
     
-    # Route tables names
+    # Spoke 1 or Single VCN Route tables names
     public_subnet_route_table_name      = "${local.public_subnet_name}-Route"
     private_subnet_app_route_table_name = "${local.private_subnet_app_name}-Route"
     private_subnet_db_route_table_name  = "${local.private_subnet_db_name}-Route"
     
+    ## Spoke 2 VCN
+
+    # Spoke 2 VCN Name
+
+    spoke2_vcn_display_name = "${var.service_label}-spoke2-vcn"
+
+    # Subnet Spoke 2 names
+    spoke2_private_subnet_web_name = "${var.service_label}-spoke2-web-subnet"
+    spoke2_private_subnet_app_name = "${var.service_label}-spoke2-app-subnet"
+    spoke2_private_subnet_db_name  = "${var.service_label}-spoke2-db-subnet"
+
+    # Spoke 2 Security lists names
+    spoke2_private_subnet_web_security_list_name = "${local.spoke2_private_subnet_web_name}-security-list"
+    spoke2_private_subnet_app_security_list_name = "${local.spoke2_private_subnet_app_name}-security-list"
+    spoke2_private_subnet_db_security_list_name  = "${local.spoke2_private_subnet_db_name}-security-list"
+
+    # Spoke 2 Network security groups names
+    spoke2_bastion_nsg_name     = "${var.service_label}-bastion-nsg"
+    spoke2_lbr_nsg_name     = "${var.service_label}-lbr-nsg"
+    spoke2_app_nsg_name     = "${var.service_label}-app-nsg"
+    spoke2_db_nsg_name      = "${var.service_label}-db-nsg"
+
+    # Spoke 1 Route tables names
+    spoke2_route_table_name = "${var.service_label}-app2-route-table"
+
     ### Object Storage
     oss_key_name = "${var.service_label}-oss-key"
     bucket_name  = "${var.service_label}-bucket"
--- config/variables.tf
+++ config/variables.tf
@@ -1,7 +1,7 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-# General
+# Environment
 variable "service_label" {
     validation {
         condition     = length(regexall("^[A-Za-z][A-Za-z0-9]{1,7}$", var.service_label)) > 0
@@ -29,7 +29,89 @@ variable "region" {
   }
 }  
 
+#Enclosing Compartment
+variable "use_enclosing_compartment" {
+    type    = bool
+    default = false
+    description = "Whether or not the Landing Zone compartments are created within an enclosing compartment. If unchecked, the Landing Zone compartments are created under the root compartment."
+}
+variable "existing_enclosing_compartment_ocid" {
+    type    = string
+    default = null
+    description = "The enclosing compartment where Landing Zone compartments will be created. If not provided and use_enclosing_compartment is true, an enclosing compartment is created under the root compartment."
+}
+variable "policies_in_root_compartment" {
+    type    = string
+    default = "CREATE"
+    description = "Whether or not required policies at the root compartment should be created or simply used. If \"CREATE\", you must be sure the user executing this stack has permissions to create policies in the root compartment. If \"USE\", policies must have been created previously."
+    validation {
+      condition = var.policies_in_root_compartment == "CREATE" || var.policies_in_root_compartment == "USE"
+      error_message = "Invalid value provided for policies_in_root_compartment. Valid values: CREATE or USE."
+  }
+}
+variable "use_existing_iam_groups" {
+    type    = bool
+    default = false
+    description = "Whether or not existing groups are to be reused for this Landing Zone. If unchecked, one set of groups is created. If checked, existing group names must be provided and this set will be able to manage resources in this Landing Zone."
+}
+variable "existing_iam_admin_group_name" {
+    type    = string
+    default = ""
+}
+variable "existing_cred_admin_group_name" {
+    type    = string
+    default = ""
+}
+variable "existing_security_admin_group_name" {
+    type    = string
+    default = ""
+}
+variable "existing_network_admin_group_name" {
+    type    = string
+    default = ""
+}
+variable "existing_appdev_admin_group_name" {
+    type    = string
+    default = ""
+}
+variable "existing_database_admin_group_name" {
+    type    = string
+    default = ""
+}
+variable "existing_auditor_group_name" {
+    type    = string
+    default = ""
+}
+variable "existing_announcement_reader_group_name" {
+    type    = string
+    default = ""
+}
+
 # Networking
+variable "hub_spoke_architecture" {
+  type        = bool
+  default     = true
+  description = "Determines if a Hub and Spoke Architecture is deployed"
+}
+variable "is_vcn_onprem_connected" {
+    default = false
+    validation {
+      condition = can(tobool(var.is_vcn_onprem_connected))
+      error_message = "Invalid value provided for is_vcn_onprem_connected. Valid values: true or false."
+    }
+}
+variable "onprem_cidr" {
+    default = "0.0.0.0/0"
+    validation { 
+        condition = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$",var.onprem_cidr)) > 0
+        error_message = "Invalid cidr block value provided for onprem_cidr variable."
+    }
+}
+variable "no_internet_access" {
+  default = false
+  type = bool
+  description = "Determines if the network will have direct access to the internet. Default is false which creates an Internet Gateway and NAT Gateway, true will not create an Internet Gateway or NAT Gateway"
+}
 variable "vcn_cidr" {
     default = "10.0.0.0/16"
     validation { 
@@ -71,21 +153,57 @@ variable "public_src_lbr_cidr" {
         error_message = "Invalid cidr block value provided for public_src_lbr_cidr variable."
     }
 }
-variable "is_vcn_onprem_connected" {
-    default = false
-    validation {
-      condition = can(tobool(var.is_vcn_onprem_connected))
-      error_message = "Invalid value provided for is_vcn_onprem_connected. Valid values: true or false."
-    }
+## DMZ VCN 
+variable "dmz_vcn_cidr" {
+  default = "10.2.0.0/20"
+  validation {
+    condition     = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.dmz_vcn_cidr)) > 0
+    error_message = "Invalid cidr block value provided for hub_vcn_cidr variable."
+  }
 }
-variable "onprem_cidr" {
-    default = "0.0.0.0/0"
-    validation { 
-        condition = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$",var.onprem_cidr)) > 0
-        error_message = "Invalid cidr block value provided for onprem_cidr variable."
-    }
+variable "dmz_bastion_subnet_cidr" {
+  default = "10.2.1.0/24"
+  validation {
+    condition     = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.dmz_bastion_subnet_cidr)) > 0
+    error_message = "Invalid cidr block value provided for hub_bastion_subnet_cidr variable."
+  }
 }
-
+variable "dmz_services_subnet_cidr" {
+  default = "10.2.2.0/24"
+  validation {
+    condition     = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.dmz_services_subnet_cidr)) > 0
+    error_message = "Invalid cidr block value provided for hub_services_subnet_cidr variable."
+  }
+}
+## Spoke 2 VCN 
+variable "spoke2_vcn_cidr" {
+  default = "10.3.0.0/20"
+  validation {
+    condition     = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.spoke2_vcn_cidr)) > 0
+    error_message = "Invalid cidr block value provided for spoke2_vcn_cidr variable."
+  }
+}
+variable "spoke2_private_subnet_web_cidr" {
+  default = "10.3.1.0/24"
+  validation {
+    condition     = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.spoke2_private_subnet_web_cidr)) > 0
+    error_message = "Invalid cidr block value provided for spoke2_private_subnet_web_cidr variable."
+  }
+}
+variable "spoke2_private_subnet_app_cidr" {
+  default = "10.3.2.0/24"
+  validation {
+    condition     = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.spoke2_private_subnet_app_cidr)) > 0
+    error_message = "Invalid cidr block value provided for spoke2_private_subnet_app_cidr variable."
+  }
+}
+variable "spoke2_private_subnet_db_cidr" {
+  default = "10.3.3.0/24"
+  validation {
+    condition     = length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.spoke2_private_subnet_db_cidr)) > 0
+    error_message = "Invalid cidr block value provided for spoke2_private_subnet_db_cidr variable."
+  }
+}
 # Monitoring
 variable "network_admin_email_endpoint" {
     validation { 
@@ -146,18 +264,6 @@ variable "service_connector_audit_target_cmpt_OCID" {
     description = "Applicable only for streaming/functions target types. OCID of compartment containing the stream/function target for the Service Connector Hub for Audit logs"
 }
 
-variable "sch_audit_target_rollover_MBs" {
-    type = number
-    default = 100
-    description = "Applicable only for objectStorage target type. Target rollover size in MBs for Audit logs"
-}
-
-variable "sch_audit_target_rollover_MSs" {
-    type = number
-    default = 7 * 60 * 1000 // 7 minutes
-    description = "Applicable only for objectStorage target type. Target rollover time in MSs for Audit logs"
-}
-
 variable "sch_audit_objStore_objNamePrefix" {
     type = string
     default = "sch-audit"
@@ -191,27 +297,15 @@ variable "service_connector_vcnFlowLogs_state" {
 }
 
 variable "service_connector_vcnFlowLogs_target_OCID" {
-    type =string
+    type = string
     default = ""
     description = "Applicable only for streaming/functions target types. OCID of stream/function target for the Service Connector Hub for VCN Flow logs"
 }
 
 variable "service_connector_vcnFlowLogs_target_cmpt_OCID" {
-    type =string
+    type = string
     default = ""
     description = "Applicable only for streaming/functions target types. OCID of compartment containing the stream/function target for the Service Connector Hub for VCN Flow logs"
-}
-
-variable "sch_vcnFlowLogs_target_rollover_MBs" {
-    type = number
-    default = 100
-    description = "Applicable only for objectStorage target type. Target rollover size in MBs for VCN Flow logs"
-}
-
-variable "sch_vcnFlowLogs_target_rollover_MSs" {
-    type = number
-    default = 7 * 60 * 1000 // 7 minutes
-    description = "Applicable only for objectStorage target type. Target rollover time in MSs for VCN Flow logs"
 }
 
 variable "sch_vcnFlowLogs_objStore_objNamePrefix" {

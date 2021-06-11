39b059c5f656cf37fb023dfa2bf08ed5b9fe67ce
Josh Hammer <josh@halimer.com>
2021-06-10 22:18:33 -0400

    Created VCN Basic and VCN Routing module to support LZ v2

--- config/net_spoke2.tf
+++ config/net_spoke2.tf
@@ -1,86 +1,193 @@
-module "cis_spoke2_vcn" {
-  depends_on           = [module.cis_vcn]
-  count                = var.hub_spoke_architecture == true ? 1 : 0
-  source               = "../modules/network/vcn"
-  compartment_id       = module.cis_compartments.compartments[local.network_compartment_name].id
-  vcn_display_name     = local.spoke2_vcn_display_name
-  vcn_cidr             = var.spoke2_vcn_cidr
-  vcn_dns_label        = "spoke2"
-  service_label        = var.service_label
-  service_gateway_cidr = local.valid_service_gateway_cidrs[0]
-  drg_id               = module.cis_vcn.drg.id
-  is_create_drg        = false
-  is_create_igw        = false
-  is_hub_spoke         = var.hub_spoke_architecture
-
-  subnets = {
-    (local.spoke2_private_subnet_web_name) = {
-      compartment_id    = null
-      defined_tags      = null
-      freeform_tags     = null
-      dynamic_cidr      = false
-      cidr              = var.spoke2_private_subnet_web_cidr
-      cidr_len          = null
-      cidr_num          = null
-      enable_dns        = true
-      dns_label         = "spoke2web"
-      private           = true
-      ad                = null
-      dhcp_options_id   = null
-      route_table_id    = module.cis_spoke2_vcn[0].subnets_route_tables[local.spoke2_route_table_name].id
-      security_list_ids = [module.cis_spoke2_security_lists[0].security_lists[local.spoke2_private_subnet_app_security_list_name].id]
-    },
-    (local.spoke2_private_subnet_app_name) = {
-      compartment_id    = null
-      defined_tags      = null
-      freeform_tags     = null
-      dynamic_cidr      = false
-      cidr              = var.spoke2_private_subnet_app_cidr
-      cidr_len          = null
-      cidr_num          = null
-      enable_dns        = true
-      dns_label         = "spoke2app"
-      private           = true
-      ad                = null
-      dhcp_options_id   = null
-      route_table_id    = module.cis_spoke2_vcn[0].subnets_route_tables[local.spoke2_route_table_name].id
-      security_list_ids = [module.cis_spoke2_security_lists[0].security_lists[local.spoke2_private_subnet_app_security_list_name].id]
-    },
-    (local.spoke2_private_subnet_db_name) = {
-      compartment_id    = null
-      defined_tags      = null
-      freeform_tags     = null
-      dynamic_cidr      = false
-      cidr              = var.spoke2_private_subnet_db_cidr
-      cidr_len          = null
-      cidr_num          = null
-      enable_dns        = true
-      dns_label         = "spoke2db"
-      private           = true
-      ad                = null
-      dhcp_options_id   = null
-      route_table_id    = module.cis_spoke2_vcn[0].subnets_route_tables[local.spoke2_route_table_name].id
-      security_list_ids = [module.cis_spoke2_security_lists[0].security_lists[local.spoke2_private_subnet_db_security_list_name].id]
+locals {
+  subnet_names = ["web", "app", "db"]
+  vcn_names = { for v in var.spoke_vcn_cidrs : "spoke${index(var.spoke_vcn_cidrs, v)}" => {
+    name : "${var.service_label}-spoke${index(var.spoke_vcn_cidrs, v)}-vcn",
+    cidr : v
     }
   }
 
-  subnets_route_tables = {
-    (local.spoke2_route_table_name) = {
-      compartment_id = null
-      route_rules = [{
+  vcns = { for key, vcn in local.vcn_names : vcn.name => {
+    compartment_id    = module.cis_compartments.compartments[local.network_compartment_name].id
+    cidr              = vcn.cidr
+    dns_label         = key
+    is_create_igw     = false
+    is_attach_drg     = true
+    block_nat_traffic = false
+    defined_tags      = null
+    subnets = { for s in local.subnet_names : "${vcn.name}-${s}-subnet" => {
+      compartment_id  = null
+      defined_tags    = null
+      cidr            = cidrsubnet(vcn.cidr, 4, index(local.subnet_names, s))
+      dns_label       =  s
+      private         = false
+      dhcp_options_id = null
+      }
+    }
+
+    }
+  }
+  vcn_ids = module.lz_spoke_vcns.vcns
+  subnets = module.lz_spoke_vcns.subnets
+  route_tables = { for key, subnet in local.subnets : replace(key,"subnet","route-table") => {
+    compartment_id = subnet.compartment_id
+    vcn_id         = subnet.vcn_id
+    subnet_id      = subnet.id
+    defined_tags   = null
+    route_rules = [{
+      is_create         = true
+      destination       = "0.0.0.0/0"
+      destination_type  = "CIDR_BLOCK"
+      network_entity_id = module.lz_spoke_vcns.drg.id
+      description       = "I wrote"
+    },
+    {
         is_create         = true
         destination       = local.valid_service_gateway_cidrs[0]
         destination_type  = "SERVICE_CIDR_BLOCK"
-        network_entity_id = module.cis_spoke2_vcn[0].service_gateway.id
-        },
-        {
-          is_create         = true
-          destination       = local.anywhere
-          destination_type  = "CIDR_BLOCK"
-          network_entity_id = module.cis_vcn.drg != null ? module.cis_vcn.drg.id : null
-        }
-      ]
+        network_entity_id = module.lz_spoke_vcns.service_gateways[subnet.vcn_id].id
+        description       = "I wrote"
+    }]
     }
   }
 }
+
+module "lz_spoke_vcns" {
+  source               = "../modules/network/vcn-basic"
+  compartment_id       = module.cis_compartments.compartments[local.network_compartment_name].id
+  service_label        = var.service_label
+  service_gateway_cidr = local.valid_service_gateway_cidrs[0]
+  is_create_drg        = true
+  vcns                 = local.vcns
+
+}
+
+
+module lz_route_tables {
+    source             = "../modules/network/vcn-routing"
+    compartment_id     = module.cis_compartments.compartments[local.network_compartment_name].id
+    subnets_route_tables = local.route_tables
+}
+#   subnets_route_tables = {
+#     ("${var.service_label}-spoke-${count.index}-route-table") = {
+#       compartment_id = null
+#       route_rules = [{
+#         is_create         = true
+#         destination       = local.valid_service_gateway_cidrs[0]
+#         destination_type  = "SERVICE_CIDR_BLOCK"
+#         network_entity_id = module.cis_spoke2_vcn[0].service_gateway.id
+#         },
+#         {
+#           is_create         = true
+#           destination       = local.anywhere
+#           destination_type  = "CIDR_BLOCK"
+#           network_entity_id = module.cis_vcn.drg != null ? module.cis_vcn.drg.id : null
+#         }
+#       ]
+#     }
+#   }
+
+
+
+
+#   subnets = {for s in range(var.dmz_number_of_subnets) : "${var.service_label}-spoke-${index(var.spoke_vcn_cidrs,v)}-web-subnet" => {
+#     compartment_id = null
+#     defined_tags = null
+#     cidr              = cidrsubnet(var.dmz_vcn_cidr,var.dmz_subnet_size,s)
+#     dns_label         = null
+#     private           = local.dmz_subnet_names[s] == "outdoor" && !var.no_internet_access ? false : true
+#     ad                = null
+#     dhcp_options_id   = null
+#   } 
+#   }
+# }
+
+
+
+# module "cis_spoke2_vcn" {
+#   # depends_on           = [module.cis_dmz_vcn]
+#   count                = length(var.spoke_vcn_cidrs) # var.hub_spoke_architecture == true ? 1 : 0
+#   source               = "../modules/network/vcn-basic"
+#   compartment_id       = module.cis_compartments.compartments[local.network_compartment_name].id
+#   vcn_display_name     = "${var.service_label}-spoke-${count.index}-vcn"
+#   vcn_cidr             = var.spoke_vcn_cidrs[count.index]
+#   vcn_dns_label        = "spoke${count.index}"
+#   service_label        = var.service_label
+#   service_gateway_cidr = local.valid_service_gateway_cidrs[0]
+# #  drg_id               = module.cis_vcn.drg.id
+#   is_create_drg        = false
+#   is_create_igw        = false
+#   is_hub_spoke         = var.hub_spoke_architecture
+#   subnets = {
+#     ("${var.service_label}-spoke-${count.index}-web-subnet") = {
+#       compartment_id    = null
+#       defined_tags      = null
+#       freeform_tags     = null
+#       dynamic_cidr      = false
+#       cidr              = cidrsubnet(var.spoke_vcn_cidrs[count.index],4,0)
+#       cidr_len          = null
+#       cidr_num          = null
+#       enable_dns        = true
+#       dns_label         = null # "spoke-${count.index}web"
+#       private           = true
+#       ad                = null
+#       dhcp_options_id   = null
+#       route_table_id    = module.cis_spoke2_vcn[count.index].subnets_route_tables["${var.service_label}-spoke-${count.index}-route-table"].id
+#       # security_list_ids = [module.cis_spoke2_security_lists[count.index].security_lists["${var.service_label}-spoke${count.index}-security-list"].id]
+#       security_list_ids = null
+#     },
+#     ("${var.service_label}-spoke-${count.index}-app-subnet") = {
+#       compartment_id    = null
+#       defined_tags      = null
+#       freeform_tags     = null
+#       dynamic_cidr      = false
+#       cidr              = cidrsubnet(var.spoke_vcn_cidrs[count.index],4,0)
+#       cidr_len          = null
+#       cidr_num          = null
+#       enable_dns        = true
+#       dns_label         = null # "spoke-${count.index}app"
+#       private           = true
+#       ad                = null
+#       dhcp_options_id   = null
+#       route_table_id    = module.cis_spoke2_vcn[0].subnets_route_tables["${var.service_label}-spoke-${count.index}-route-table"].id
+#       #security_list_ids = [module.cis_spoke2_security_lists[count.index].security_lists["${var.service_label}-spoke${count.index}-security-list"].id]
+#       security_list_ids = null
+#     },
+#     ("${var.service_label}-spoke-${count.index}-db-subnet") = {
+#       compartment_id    = null
+#       defined_tags      = null
+#       freeform_tags     = null
+#       dynamic_cidr      = false
+#       cidr              = cidrsubnet(var.spoke_vcn_cidrs[count.index],4,0)
+#       cidr_len          = null
+#       cidr_num          = null
+#       enable_dns        = true
+#       dns_label         = null # "spoke-${count.index}db"
+#       private           = true
+#       ad                = null
+#       dhcp_options_id   = null
+#       route_table_id    = module.cis_spoke2_vcn[0].subnets_route_tables["${var.service_label}-spoke-${count.index}-route-table"].id
+#       security_list_ids = [module.cis_spoke2_security_lists[0].security_lists["${var.service_label}-spoke${count.index}-security-list"].id]
+#       security_list_ids = null
+#     }
+#   }
+
+#   subnets_route_tables = {
+#     ("${var.service_label}-spoke-${count.index}-route-table") = {
+#       compartment_id = null
+#       route_rules = [{
+#         is_create         = true
+#         destination       = local.valid_service_gateway_cidrs[0]
+#         destination_type  = "SERVICE_CIDR_BLOCK"
+#         network_entity_id = module.cis_spoke2_vcn[0].service_gateway.id
+#         },
+#         {
+#           is_create         = true
+#           destination       = local.anywhere
+#           destination_type  = "CIDR_BLOCK"
+#           network_entity_id = module.cis_vcn.drg != null ? module.cis_vcn.drg.id : null
+#         }
+#       ]
+#     }
+#   }
+# }
 
---
+++ modules/network/vcn-basic/main.tf
@@ -0,0 +1,86 @@
+locals {
+  osn_cidrs = {for x in data.oci_core_services.all_services.services : x.cidr_block => x.id}
+
+  subnets = flatten([
+    for k, v in var.vcns : [
+      for k1, v1 in v.subnets : {
+        vcn_name        = k 
+        display_name    = k1
+        cidr            = v1.cidr
+        compartment_id  = v1.compartment_id != null ? v1.compartment_id : var.compartment_id
+        private         = v1.private
+        dns_label       = v1.dns_label
+        dhcp_options_id = v1.dhcp_options_id
+        defined_tags    = v1.defined_tags
+      }
+    ]
+  ])
+}
+
+data "oci_core_services" "all_services" {
+}
+
+### VCN
+resource "oci_core_vcn" "these" {
+  for_each = var.vcns
+    display_name   = each.key
+    dns_label      = each.value.dns_label
+    cidr_block     = each.value.cidr
+    compartment_id = each.value.compartment_id
+}
+
+### Internet Gateway
+resource "oci_core_internet_gateway" "these" {
+  for_each = {for k, v in var.vcns: k => v if v.is_create_igw == true}
+    compartment_id = each.value.compartment_id
+    vcn_id         = oci_core_vcn.these[each.key].id
+    display_name   = "${each.key}-int-gw"
+}
+
+### NAT Gateway
+resource "oci_core_nat_gateway" "these" {
+  for_each = {for k, v in var.vcns: k => v if v.is_create_igw == true}
+    compartment_id = each.value.compartment_id
+    display_name  = "${each.key}-nat-gw"
+    vcn_id        = oci_core_vcn.these[each.key].id
+    block_traffic = each.value.block_nat_traffic
+}
+
+### Service Gateway
+resource "oci_core_service_gateway" "these" {
+  for_each = var.vcns
+    compartment_id = each.value.compartment_id
+    display_name   = "${each.key}-svc-gw"
+    vcn_id         = oci_core_vcn.these[each.key].id
+    services {
+      service_id = local.osn_cidrs[var.service_gateway_cidr]
+    }
+}
+
+### DRG - Dynamic Routing Gateway
+resource "oci_core_drg" "this" {
+  count          = var.is_create_drg == true ? 1 : 0
+  compartment_id = var.compartment_id
+  display_name   = "${var.service_label}-drg"
+}
+
+### DRG attachment to VCN
+resource "oci_core_drg_attachment" "these" {
+  for_each = {for k, v in var.vcns: k => v if v.is_attach_drg == true}
+    drg_id       = oci_core_drg.this[0].id
+    vcn_id       = oci_core_vcn.these[each.key].id
+    display_name = "${each.key}-drg-attachment"
+}
+
+### Subnets
+resource "oci_core_subnet" "these" {
+  for_each = {for subnet in local.subnets : "${subnet.vcn_name}.${subnet.display_name}" => subnet}
+    display_name                = each.value.display_name
+    vcn_id                      = oci_core_vcn.these[each.value.vcn_name].id
+    cidr_block                  = each.value.cidr
+    compartment_id              = each.value.compartment_id
+    prohibit_public_ip_on_vnic  = each.value.private
+    dns_label                   = each.value.dns_label
+    dhcp_options_id             = each.value.dhcp_options_id
+    defined_tags                = each.value.defined_tags
+}

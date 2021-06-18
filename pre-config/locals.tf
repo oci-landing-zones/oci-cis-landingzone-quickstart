002b26c0a9ae8cd2c0484b88e7b7d9a23009232e
Andre Correa <andre.correa@oracle.com>
2021-06-18 10:23:26 -0300

    Policy names updated.

--- pre-config/locals.tf
+++ pre-config/locals.tf
@@ -3,8 +3,8 @@ locals {
   top_compartment_parent_id  = length(var.existing_enclosing_compartments_parent_ocid) > 0 ? var.existing_enclosing_compartments_parent_ocid : var.tenancy_ocid
   enclosing_compartments     = length(var.enclosing_compartment_names) > 0 ? {for c in var.enclosing_compartment_names : "${var.unique_prefix}-${c}" => {parent_id: local.top_compartment_parent_id, description: "Landing Zone enclosing compartment"}} : {"${var.unique_prefix}-top-cmp": {parent_id: local.top_compartment_parent_id, description: "Landing Zone enclosing compartment"}}
   provisioning_group_names   = {for k in keys(local.enclosing_compartments) : k => {group_name: var.use_existing_provisioning_group == false ? "${k}-prov-group" : var.existing_provisioning_group_name}}
-  provisioning_group_names_t = var.use_existing_provisioning_group == false ? {for k in keys(local.enclosing_compartments) : k => {group_name:"${k}-prov-group"}} : {"root" : {group_name: var.existing_provisioning_group_name}}
-  lz_group_names             = var.use_existing_lz_groups == false ? {for k in keys(local.enclosing_compartments) : k => {group_name_prefix:"${k}-"}} : {"root": {group_name_prefix: ""}}
+  provisioning_group_names_t = var.use_existing_provisioning_group == false ? {for k in keys(local.enclosing_compartments) : k => {group_name:"${k}-prov-group"}} : {(var.unique_prefix) : {group_name: var.existing_provisioning_group_name}}
+  lz_group_names             = var.use_existing_lz_groups == false ? {for k in keys(local.enclosing_compartments) : k => {group_name_prefix:"${k}-"}} : {(var.unique_prefix) : {group_name_prefix: ""}}
 
   iam_admin_group_name_suffix           = var.use_existing_lz_groups == false ? "iam-admin-group" : var.existing_iam_admin_group_name
   cred_admin_group_name_suffix          = var.use_existing_lz_groups == false ? "cred-admin-group" : var.existing_iam_admin_group_name

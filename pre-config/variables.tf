3e2d7df8136c84841525c24fcc8cf2e1db5c5107
Andre Correa <andre.correa@oracle.com>
2021-06-18 10:20:14 -0300

    unique prefix max length changed to 16

--- pre-config/variables.tf
+++ pre-config/variables.tf
@@ -19,14 +19,14 @@ variable "unique_prefix" {
   type = string
   description = "A unique prefix across the tenancy that is added to all resource names created by this module."  
   validation {
-    condition     = length(regexall("^[A-Za-z][A-Za-z0-9]{1,7}$", var.unique_prefix)) > 0
-    error_message = "The unique_prefix variable is required and must contain alphanumeric characters only, start with a letter and 8 character max."
+    condition     = length(regexall("^[A-Za-z][A-Za-z0-9]{1,15}$", var.unique_prefix)) > 0
+    error_message = "The unique_prefix variable is required and must contain alphanumeric characters only, start with a letter and 16 character max."
   }
 }
 variable "use_existing_provisioning_group" {
     type = bool
     default = false
-    description = "Whether or not an existing group will be used for Landing Zone provisioning. If unchecked, one group is created for each compartment defined by enclosing_compartment_names variable."
+    description = "Whether or not an existing group will be used for Landing Zone provisioning. If false, one group is created for each compartment defined by enclosing_compartment_names variable."
 }
 variable "existing_provisioning_group_name" {
     type    = string
@@ -51,6 +51,11 @@ variable "use_existing_lz_groups" {
     type = bool
     default = false
     description = "Whether or not existing groups are to be reused for Landing Zone. If unchecked, one set of groups is created for each compartment defined by enclosing_compartment_names variable. If checked, existing group names must be provided and this single set will be able to manage resources in all those compartments."
+}
+variable "create_services_policies" {
+    type = bool
+    default = true
+    description = "Whether services policies should be created. If these policies already exist in the root compartment, set it to false for avoiding policies duplication. Services policies are required by some OCI services, like Cloud Guard, Vulnerability Scanning and OS Management."
 }
 variable "create_tenancy_level_policies" {
     type = bool

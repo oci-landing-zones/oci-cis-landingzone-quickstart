0c5a057c5c27ae73f964945b4c23f6e1d396f8a7
Andre Correa <andre.correa@oracle.com>
2021-06-23 17:37:29 -0300

    validation for dmz_vcn_cidr added

--- config/variables.tf
+++ config/variables.tf
@@ -138,10 +138,11 @@ variable "dmz_vcn_cidr" {
   type        = string
   default     = null
   description = "CIDR block for the DMZ VCN.  DMZ VCNs are commonly used for network appliance deployments. All traffic will be routed through the DMZ."
-  #validation {
-    #condition     =  var.dmz_vcn_cidr == null || length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.dmz_vcn_cidr)) > 0
-    #error_message = "Validation failed for dmz_vcn_cidr: value must be in CIDR notation."
-  #}
+  validation {
+    condition = var.dmz_vcn_cidr == null ? true : (length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.dmz_vcn_cidr)) > 0 ? true : false)
+    #condition     = length(var.dmz_vcn_cidr) == 0 || length(regexall("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$", var.dmz_vcn_cidr)) > 0
+    error_message = "Validation failed for dmz_vcn_cidr: value must be in CIDR notation."
+  }
 }
 
 variable "dmz_number_of_subnets" {

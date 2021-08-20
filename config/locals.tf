b99906f1afb1942a792395b81f5182592d4dcc28
Andre Correa <andre.correa@oracle.com>
2021-08-20 16:06:08 -0300

    disabled compartment deletion upon destruction

--- config/locals.tf
+++ config/locals.tf
@@ -17,7 +17,7 @@ locals {
   database_compartment_name          = "${var.service_label}-database-cmp"
   appdev_compartment_name            = "${var.service_label}-appdev-cmp"
   # Whether compartments should be deleted in terraform destroy or upon resource removal.
-  enable_cmp_delete = true
+  enable_cmp_delete = false
 
   # Whether or not to create an enclosing compartment
   parent_compartment_id         = var.use_enclosing_compartment == true ? (var.existing_enclosing_compartment_ocid != null ? var.existing_enclosing_compartment_ocid : module.lz_top_compartment[0].compartments[local.default_enclosing_compartment_name].id) : var.tenancy_ocid

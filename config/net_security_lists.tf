e55fcd656a1f654c93816345baed3887b578ea23
Halimer <josh.hammer@oracle.com>
2021-08-24 15:54:59 -0400

    Updated Default Security Lists with ExaCS VCNs

--- config/net_security_lists.tf
+++ config/net_security_lists.tf
@@ -5,8 +5,15 @@
 ### The security rules are driven by NSGs (Network Security Groups). See net_nsgs.tf
 ### Add security rules as needed.
 
+locals {
+  sl_exas_vcns = length(var.exacs_vcn_cidrs) > 0 ? module.lz_exacs_vcns.vcns : {}
+  sl_dmz_vcns = length(var.dmz_vcn_cidr) > 0 ? module.lz_vcn_dmz.vcns : {}
+  sl_all_vcns = merge(local.sl_exas_vcns, local.sl_dmz_vcns, module.lz_vcn_spokes.vcns)
+}
+
+
 resource "oci_core_default_security_list" "default_security_list" {
-  for_each                   = length(var.dmz_vcn_cidr) == 0 ? module.lz_vcn_spokes.vcns : merge(module.lz_vcn_spokes.vcns, module.lz_vcn_dmz.vcns)
+  for_each                   = local.sl_all_vcns
   manage_default_resource_id = each.value.default_security_list_id
   ingress_security_rules {
     protocol  = "1"

98a801c13ea9137dcaa4a1c91bebd98137a0a3ff
Andre Correa <andre.correa@oracle.com>
2021-09-13 14:46:13 -0300

    Exa and ADB-D admin groups added

--- pre-config/lz_groups.tf
+++ pre-config/lz_groups.tf
@@ -60,6 +60,16 @@ module "lz_groups" {
         description = "Landing Zone group for reading tenancy announcements."
         user_ids = []
         defined_tags = null
+      },
+      "${each.value.group_name_prefix}${local.exainfra_admin_group_name_suffix}" = {
+        description = "Landing Zone group for managing Exadata Cloud service infrastructures."
+        user_ids = []
+        defined_tags = null
+      },
+      "${each.value.group_name_prefix}${local.adbexainfra_admin_group_name_suffix}" = {
+        description = "Landing Zone group for managing ADB-Dedicated Exadata infrastructures."
+        user_ids = []
+        defined_tags = null
       }
     } 
 }

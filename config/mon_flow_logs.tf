9149253c0dcf3dded84f3c40b68f56763189e486
Andre Correa <andre.correa@oracle.com>
2021-09-13 09:27:28 -0300

    Added Exa subnets to flow logs

--- config/mon_flow_logs.tf
+++ config/mon_flow_logs.tf
@@ -3,7 +3,7 @@
 
 ### This Terraform configuration provisions flow logs for all subnets provisioned in the cis-network configuration.
 locals {
-  all_lz_subnets = var.dmz_vcn_cidr == null ? module.lz_vcn_spokes.subnets : merge(module.lz_vcn_spokes.subnets, module.lz_vcn_dmz.subnets)
+  all_lz_subnets = merge(module.lz_vcn_spokes.subnets, module.lz_vcn_dmz.subnets, module.lz_exacs_vcns.subnets)  
 
   flow_logs = { for k, v in local.all_lz_subnets : k =>
     {
@@ -22,7 +22,7 @@ locals {
   }
 }
 module "lz_flow_logs" {
-  depends_on             = [module.lz_vcn_spokes, module.lz_vcn_dmz]
+  depends_on             = [module.lz_vcn_spokes, module.lz_vcn_dmz, module.lz_exacs_vcns]
   source                 = "../modules/monitoring/logs"
   compartment_id         = module.lz_compartments.compartments[local.security_compartment_name].id
   log_group_display_name = "${var.service_label}-flow-logs-group"

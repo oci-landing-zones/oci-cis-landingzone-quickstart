c7cb7703e6acf2341b57f8b7c53516dac5d06a85
Andre Correa <andre.correa@oracle.com>
2021-09-15 14:50:15 -0300

    vcn_cidrs made optional, validation check changed.

--- config/net_drg.tf
+++ config/net_drg.tf
@@ -1,7 +1,7 @@
 module "lz_drg" {
   source         = "../modules/network/drg"
   depends_on     = [ null_resource.slow_down_drg ]
-  compartment_id = module.lz_compartments.compartments[local.network_compartment.name].id
+  compartment_id = module.lz_compartments.compartments[local.network_compartment.key].id
   service_label  = var.service_label
   is_create_drg  = (length(var.onprem_cidrs) > 0 || var.hub_spoke_architecture) && var.existing_drg_id == ""
 }

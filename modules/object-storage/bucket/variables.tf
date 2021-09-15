161698c4e2428f841a398ec6b0d813278bd36d35
Andre Correa <andre.correa@oracle.com>
2021-09-15 11:39:39 -0300

    Unnecessary variables removed.

--- modules/object-storage/bucket/variables.tf
+++ modules/object-storage/bucket/variables.tf
@@ -1,24 +1,6 @@
 # Copyright (c) 2020 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-variable "region" {
-  type        = string
-  description = "Region"
-  default     = ""
-} 
-
-variable "tenancy_ocid" {
-  type        = string
-  description = "Tenancy OCID"
-  default     = ""
-} 
-
-variable "compartment_name" {
-  type        = string
-  description = "Compartment Name"
-  default     = ""
-}  
-
 variable "kms_key_id" {
   type        = string
   description = "KMS Key ID"
@@ -29,6 +11,8 @@ variable "kms_key_id" {
 variable "buckets" {
   type = map(object({
     compartment_id = string,
+    name           = string,
+    namespace      = string
   }))
 }
   

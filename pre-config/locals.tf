018f3ff060294bdb27ab3e38d0d9716134364ab3
Jonathan Hult <jhult@mythics.com>
2021-09-23 11:56:20 -0400

    Fix variable names - use-existing-groups-when-using-pre-config case

--- pre-config/locals.tf
+++ pre-config/locals.tf
@@ -12,13 +12,13 @@ locals {
   lz_group_names           = length(local.enclosing_compartments) > 0 ? { for k in keys(local.enclosing_compartments) : k => { group_name_prefix : "${k}-" } } : { (local.unique_prefix) : { group_name_prefix : var.use_existing_groups == false ? "${local.unique_prefix}-" : "" } }
 
   iam_admin_group_name_suffix           = var.use_existing_groups == false ? "iam-admin-group" : var.existing_iam_admin_group_name
-  cred_admin_group_name_suffix          = var.use_existing_groups == false ? "cred-admin-group" : var.existing_iam_admin_group_name
-  network_admin_group_name_suffix       = var.use_existing_groups == false ? "network-admin-group" : var.existing_iam_admin_group_name
-  security_admin_group_name_suffix      = var.use_existing_groups == false ? "security-admin-group" : var.existing_iam_admin_group_name
-  appdev_admin_group_name_suffix        = var.use_existing_groups == false ? "appdev-admin-group" : var.existing_iam_admin_group_name
-  database_admin_group_name_suffix      = var.use_existing_groups == false ? "database-admin-group" : var.existing_iam_admin_group_name
-  auditor_group_name_suffix             = var.use_existing_groups == false ? "auditor-group" : var.existing_iam_admin_group_name
-  announcement_reader_group_name_suffix = var.use_existing_groups == false ? "announcement-reader-group" : var.existing_iam_admin_group_name
+  cred_admin_group_name_suffix          = var.use_existing_groups == false ? "cred-admin-group" : var.existing_cred_admin_group_name
+  network_admin_group_name_suffix       = var.use_existing_groups == false ? "network-admin-group" : var.existing_network_admin_group_name
+  security_admin_group_name_suffix      = var.use_existing_groups == false ? "security-admin-group" : var.existing_security_admin_group_name
+  appdev_admin_group_name_suffix        = var.use_existing_groups == false ? "appdev-admin-group" : var.existing_appdev_admin_group_name
+  database_admin_group_name_suffix      = var.use_existing_groups == false ? "database-admin-group" : var.existing_database_admin_group_name
+  auditor_group_name_suffix             = var.use_existing_groups == false ? "auditor-group" : var.existing_auditor_group_name
+  announcement_reader_group_name_suffix = var.use_existing_groups == false ? "announcement-reader-group" : var.existing_announcement_reader_group_name
 
   grant_tenancy_level_mgmt_policies = true
   services_policy_name              = "${local.unique_prefix}-services-policy"

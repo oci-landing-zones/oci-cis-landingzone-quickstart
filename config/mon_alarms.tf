1e68e88883c7060998e2c478561a2275b0258c34
Andre Correa <andre.correa@oracle.com>
2021-10-25 18:14:04 +0000

    Deleted commented out code.

--- config/mon_alarms.tf
+++ config/mon_alarms.tf
@@ -1,26 +1,7 @@
 # Copyright (c) 2021 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-### This Terraform configuration provisions alarms for the cis tenancy.
-
-### Reference module example
-# module "lz_compute_alarms" {
-#     source    = "../modules/monitoring/alarms"
-#     alarms = {
-#         ("${var.service_label}-high-cpu-alarm") = {
-#            compartment_id = var.tenancy_ocid
-#            destinations = [module.lz_compute_topic.topic.id]
-#            display_name = "${var.service_label}-high-cpu-alarm"
-#            is_enabled = var.create_alarms_as_enabled
-#            metric_compartment_id = var.tenancy_ocid
-#            namespace = "oci_computeagent"
-#            query = "CpuUtilization[5m].mean() > 80"
-#            severity = "critical"
-
-        
-#         }
-
-
+### This Terraform configuration provisions alarms for the tenancy.
 
 module "lz_compute_alarms" {
     source    = "../modules/monitoring/alarms"
@@ -132,43 +113,6 @@ module "lz_database_alarms" {
         
     }
 }
-
-
-# module "lz_storage_alarms" {
-#     source    = "../modules/monitoring/alarms"
-#     alarms = {
-#         ("${var.service_label}-oss-failed-upload-alarm") = {
-#            compartment_id = local.appdev_compartment.name
-#            destinations = [module.lz_storage_topic.topic.id] 
-#            display_name = "${var.service_label}-oss-failed-upload-alarm"
-#            is_enabled = var.create_alarms_as_enabled
-#            metric_compartment_id = local.appdev_compartment.name
-#            namespace = "oci_objectstorage"
-#            query = "UncommittedParts[1m].bytes() > 5000"
-#            severity = "warning"
-#            metric_compartment_id_in_subtree = true
-#         }
-        
-#     }
-# }
-
-# module "lz_security_alarms" {
-#     source    = "../modules/monitoring/alarms"
-#     alarms = {
-       
-#         ("${var.service_label}-vulnerability-scanning-alarm") = {
-#            compartment_id = var.tenancy_ocid
-#            destinations = [oci_ons_notification_topic.test_notification_topic.id] 
-#            display_name = var.alarm_display_name
-#            is_enabled = var.alarm_is_enabled
-#            metric_compartment_id = var.alarm_metric_compartment_id
-#            namespace = var.alarm_namespace
-#            query = var.alarm_query
-#            severity = var.alarm_severity
-#         }
-        
-#     }
-# }
 
 module "lz_network_alarms" {
     source    = "../modules/monitoring/alarms"

c77f2435c0021e1ca9c759c1f339f773bce9ec18
Halimer <josh.hammer@oracle.com>
2021-10-26 10:14:00 -0400

    Added outputs for the alarms module

--- modules/monitoring/alarms/outputs.tf
+++ modules/monitoring/alarms/outputs.tf
@@ -1,7 +1,8 @@
-# Copyright (c) 2020 Oracle and/or its affiliates.
+# Copyright (c) 2021 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 
-
-
-
+output alarms {
+    description = "The alarms, indexed by display name."
+    value = {for alarm in oci_monitoring_alarm.these : alarm.display_name => alarm}
+}

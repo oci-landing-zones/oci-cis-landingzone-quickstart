ce5a5aecd27b849e83b0f627b94b48fa0b243096
Johannes Murmann <johannes.murmann@oracle.com>
2021-11-02 16:53:16 -0700

    changed count to map.

--- modules/cost/budget/main.tf
+++ modules/cost/budget/main.tf
@@ -2,24 +2,28 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 resource oci_budget_budget this {
-  amount                                = var.budget_amount
+  for_each = var.budget
+  amount                                = each.value.budget_amount
   budget_processing_period_start_offset = "1"
-  compartment_id                        = var.tenancy_id
-  description  = var.budget_description
-  display_name = var.budget_display_name
-  reset_period = "MONTHLY"
-  target_type  = "COMPARTMENT"
+  compartment_id                        = each.value.tenancy_id
+  description                           = each.value.budget_description
+  display_name                          = each.key
+  reset_period                          = "MONTHLY"
+  target_type                           = "COMPARTMENT"
   targets = [
-    var.compartment_id,
+    each.value.compartment_id,
   ]
 }
 
+
+
 resource oci_budget_alert_rule this {
-  budget_id = oci_budget_budget.this.id
-  display_name = "${var.service_label}-alert-on-forecasted-spent"
+  for_each = var.budget
+  budget_id       = oci_budget_budget.this[each.key].id
+  display_name    = "${each.value.service_label}-alert-on-forecasted-spent"
   #message = ""
-  recipients     = var.budget_alert_recipients
-  threshold      = var.budget_alert_threshold
-  threshold_type = "PERCENTAGE"
-  type           = "FORECAST"
+  recipients      = each.value.budget_alert_recipients
+  threshold       = each.value.budget_alert_threshold
+  threshold_type  = "PERCENTAGE"
+  type            = "FORECAST"
 }

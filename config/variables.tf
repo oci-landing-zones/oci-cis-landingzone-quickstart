4348bd880e57460c4ba0830326aa18126f676e99
Johannes Murmann <johannes.murmann@oracle.com>
2021-11-02 14:11:42 -0700

    added groups info

--- config/variables.tf
+++ config/variables.tf
@@ -90,6 +90,10 @@ variable "existing_exainfra_admin_group_name" {
   type    = string
   default = ""
 }
+variable "existing_cost_admin_group_name" {
+  type    = string
+  default = ""
+}
 
 # Networking
 variable "no_internet_access" {
@@ -281,13 +285,13 @@ variable "security_admin_email_endpoints" {
   }
 }
 
-variable "governance_admin_email_endpoints" {
+variable "cost_admin_email_endpoints" {
   type        = list(string)
   default     = []
-  description = "List of email addresses for all governance related notifications."
+  description = "List of email addresses for all cost related notifications."
   validation {
-    condition     = length([for e in var.governance_admin_email_endpoints : e if length(regexall("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", e)) > 0]) == length(var.governance_admin_email_endpoints)
-    error_message = "Validation failed governance_admin_email_endpoints: invalid email address."
+    condition     = length([for e in var.cost_admin_email_endpoints : e if length(regexall("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", e)) > 0]) == length(var.cost_admin_email_endpoints)
+    error_message = "Validation failed cost_admin_email_endpoints: invalid email address."
   }
 }
 variable "cloud_guard_configuration_status" {
@@ -402,20 +406,20 @@ variable "vss_scan_day" {
   }
 }
 
-# Governance
+# Cost Management
 variable "budget_alert_threshold" {
   type        = number
   default     = 75
-  description = "The threshold for triggering the alert expressed as a whole number or decimal value. 75% is the default."
+  description = "The threshold for triggering the alert expressed as a percentage. 75% is the default."
   validation {
-    condition     = var.budget_alert_threshold > 0 && var.budget_alert_threshold < 9999
-    error_message = "Validation failed for budget_alert_threshold: Values should be between 1 and 9999."
+    condition     = var.budget_alert_threshold > 0 && var.budget_alert_threshold < 10000
+    error_message = "Validation failed for budget_alert_threshold: The threshold percentage should be greater than 0 and less than or equal to 10,000, with no leading zeros and a maximum of 2 decimal places."
    }
 }
 
 variable "budget_amount" {
   type        = number
-  default     = 75
+  default     = 1000
   description = "The amount of the budget expressed as a whole number in the currency of the customer's rate card"
 }
 

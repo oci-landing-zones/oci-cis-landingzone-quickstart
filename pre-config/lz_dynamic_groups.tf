8d27b4e057f31ca56856dd5b24e39a43b2e7365f
Olaf Heimburger <olaf.heimburger@oracle.com>
2021-11-05 11:44:27 +0100

    Refactored dynamic_groups attribute to use local.default_dynamic_groups.

--- pre-config/lz_dynamic_groups.tf
+++ pre-config/lz_dynamic_groups.tf
@@ -1,11 +1,8 @@
 # Copyright (c) 2021 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-module "lz_dynamic_groups" {
-  source     = "../modules/iam/iam-dynamic-group"
-  depends_on = [module.lz_top_compartments]
-  for_each   = local.enclosing_compartments
-  dynamic_groups = {
+locals {
+  default_dynamic_groups = {
     ("${each.key}-fun-dynamic-group") = {
       compartment_id = var.tenancy_ocid
       description    = "Landing Zone dynamic group for functions in compartment ${each.key}"
@@ -17,5 +14,12 @@ module "lz_dynamic_groups" {
       matching_rule  = "ALL {resource.type = 'autonomousdatabase',resource.compartment.id = '${module.lz_top_compartments.compartments[each.key].id}'}"
     }
   }
+}
+
+module "lz_dynamic_groups" {
+  source     = "../modules/iam/iam-dynamic-group"
+  depends_on = [module.lz_top_compartments]
+  for_each   = local.enclosing_compartments
+  dynamic_groups = default_dynamic_groups
 }
 

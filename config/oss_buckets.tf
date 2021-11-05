1be164e5d67f638be68ef4827e63793077d83249
Olaf Heimburger <olaf.heimburger@oracle.com>
2021-11-05 11:04:05 +0100

    Refactored buckets to local.default_buckets.

--- config/oss_buckets.tf
+++ config/oss_buckets.tf
@@ -2,17 +2,22 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 ### Creates a bucket in the specified compartment 
-module "lz_buckets" {
-    depends_on   = [ null_resource.slow_down_oss ]
-    source       = "../modules/object-storage/bucket"
-    kms_key_id   = module.lz_keys.keys[local.oss_key_name].id
-    buckets      = { 
+
+locals {
+    default_buckets = { 
         ("${var.service_label}-appdev-bucket") = {
             compartment_id = module.lz_compartments.compartments[local.appdev_compartment.key].id
             name = "${var.service_label}-appdev-bucket"
             namespace = data.oci_objectstorage_namespace.this.namespace
         }
     }
+}
+
+module "lz_buckets" {
+    depends_on   = [ null_resource.slow_down_oss ]
+    source       = "../modules/object-storage/bucket"
+    kms_key_id   = module.lz_keys.keys[local.oss_key_name].id
+    buckets      = local.default_buckets
 }
 
 resource "null_resource" "slow_down_oss" {

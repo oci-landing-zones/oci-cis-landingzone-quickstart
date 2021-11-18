3dbd4e8d5cf5aca9a021b93fb22fc9fe79e1f49b
Josh Hammer <josh@halimer.com>
2021-11-18 16:47:15 -0500

    Updated output to be indexed by key instead of name

--- modules/monitoring/alarms/outputs.tf
+++ modules/monitoring/alarms/outputs.tf
@@ -2,7 +2,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 
-output alarms {
-    description = "The alarms, indexed by display name."
-    value = {for alarm in oci_monitoring_alarm.these : alarm.display_name => alarm}
-}
+output "alarms" {
+  description = "The topcs, indexed by keys in var.topics."
+  value = {for k, v in var.alarms : k => oci_monitoring_alarm.these[k]}
+} 

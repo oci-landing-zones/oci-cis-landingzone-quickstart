2bb95edb8cb9541e14d9d1c6321991a63514f6ab
Andre Correa <andre.correa@oracle.com>
2022-01-05 11:51:08 -0300

    Updated logic for LZ extension. Cmps are only created if extend_landing_zone_to_new_region is false

--- config/iam_compartments.tf
+++ config/iam_compartments.tf
@@ -8,9 +8,9 @@ module "lz_top_compartment" {
   source    = "../modules/iam/iam-compartment"
   providers = { oci = oci.home }
   compartments = {
-    (local.default_enclosing_compartment.key) = {
+    (local.enclosing_compartment.key) = {
       parent_id     = var.tenancy_ocid
-      name          = local.default_enclosing_compartment.name
+      name          = local.enclosing_compartment.name
       description   = "Landing Zone enclosing compartment, enclosing all Landing Zone compartments."
       enable_delete = local.enable_cmp_delete
       defined_tags  = null
@@ -22,45 +22,45 @@ module "lz_top_compartment" {
 locals {
   default_cmps = merge(
     {for i in [1]: (local.security_compartment.key) => {
-      parent_id     = local.parent_compartment_id
+      parent_id     = local.enclosing_compartment_id
       name          = local.security_compartment.name
       description   = "Landing Zone compartment for all security related resources: vaults, topics, notifications, logging, scanning, and others."
       enable_delete = local.enable_cmp_delete
       defined_tags  = null
       freeform_tags = null
-    } if var.existing_security_cmp_ocid == null},
+    }},
 
     {for i in [1]: (local.network_compartment.key) => {
-      parent_id     = local.parent_compartment_id
+      parent_id     = local.enclosing_compartment_id
       name          = local.network_compartment.name
       description   = "Landing Zone compartment for all network related resources: VCNs, subnets, network gateways, security lists, NSGs, load balancers, VNICs, and others."
       enable_delete = local.enable_cmp_delete
       defined_tags  = null
       freeform_tags = null
-    } if var.existing_network_cmp_ocid == null},
+    }},
 
     {for i in [1]: (local.appdev_compartment.key) => {
-      parent_id     = local.parent_compartment_id
+      parent_id     = local.enclosing_compartment_id
       name          = local.appdev_compartment.name
       description   = "Landing Zone compartment for all resources related to application development: compute instances, storage, functions, OKE, API Gateway, streaming, and others."
       enable_delete = local.enable_cmp_delete
       defined_tags  = null
       freeform_tags = null
-    } if var.existing_appdev_cmp_ocid == null},
+    }},
 
     {for i in [1]: (local.database_compartment.key) => {
-      parent_id     = local.parent_compartment_id
+      parent_id     = local.enclosing_compartment_id
       name          = local.database_compartment.name
       description   = "Landing Zone compartment for all database related resources."
       enable_delete = local.enable_cmp_delete
       defined_tags  = null
       freeform_tags = null
-    } if var.existing_database_cmp_ocid == null}
+    }}
   )
 
   exainfra_cmp = var.deploy_exainfra_cmp == true ? {
     (local.exainfra_compartment.key) = {
-      parent_id     = local.parent_compartment_id
+      parent_id     = local.enclosing_compartment_id
       name          = local.exainfra_compartment.name
       description   = "Landing Zone compartment for Exadata infrastructure."
       enable_delete = local.enable_cmp_delete
@@ -75,5 +75,5 @@ locals {
 module "lz_compartments" {
   source    = "../modules/iam/iam-compartment"
   providers = { oci = oci.home }
-  compartments = local.cmps
+  compartments = var.extend_landing_zone_to_new_region == false ? local.cmps : {}
 }

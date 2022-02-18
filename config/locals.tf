28569dbf15aca018322359f01f371d39e5f18329
Olaf Heimburger <olaf.heimburger@oracle.com>
2022-02-18 13:07:36 +0100

    "Hard wired" group name changed to local variable.

--- config/locals.tf
+++ config/locals.tf
@@ -64,8 +64,8 @@ locals {
   exainfra_admin_policy_name      = "${var.service_label}-exainfra-admin-policy"
   cost_admin_root_policy_name  = "${var.service_label}-cost-admin-root-policy"
 
-  database_kms_statements = ["Allow dynamic-group ${var.service_label}-database-kms-dynamic-group to manage vaults in compartment ${local.security_compartment.name}",
-        "Allow dynamic-group ${var.service_label}-database-kms-dynamic-group to manage vaults in compartment ${local.security_compartment.name}"]
+  database_kms_statements = ["Allow dynamic-group ${local.database_kms_dynamic_group_name} to manage vaults in compartment ${local.security_compartment.name}",
+        "Allow dynamic-group ${local.database_kms_dynamic_group_name} to manage vaults in compartment ${local.security_compartment.name}"]
 
 
   # Tags

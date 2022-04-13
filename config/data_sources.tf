72783341998e2f8ccfe1a15846b35753f49ddd8e
Andre Correa <andre.correa@oracle.com>
2022-04-13 12:22:57 -0300

    Drop down UI control for existing groups

--- config/data_sources.tf
+++ config/data_sources.tf
@@ -11,12 +11,20 @@ data "oci_identity_compartment" "existing_enclosing_compartment" {
   id = var.existing_enclosing_compartment_ocid != null ? var.existing_enclosing_compartment_ocid : var.tenancy_ocid
 }
 
+data "oci_identity_group" "existing_iam_admin_group" {
+  group_id = length(trimspace(var.existing_iam_admin_group_name)) > 0 ? var.existing_iam_admin_group_name : "nogroup"
+}
+
 data "oci_identity_groups" "existing_iam_admin_group" {
   compartment_id = var.tenancy_ocid
   filter {
     name   = "name"
     values = [var.existing_iam_admin_group_name]
   }
+}
+
+data "oci_identity_group" "existing_cred_admin_group" {
+  group_id = length(trimspace(var.existing_cred_admin_group_name)) > 0 ? var.existing_cred_admin_group_name : "nogroup"
 }
 
 data "oci_identity_groups" "existing_cred_admin_group" {
@@ -27,12 +35,20 @@ data "oci_identity_groups" "existing_cred_admin_group" {
   }
 }
 
+data "oci_identity_group" "existing_security_admin_group" {
+  group_id = length(trimspace(var.existing_security_admin_group_name)) > 0 ? var.existing_security_admin_group_name : "nogroup"
+}
+
 data "oci_identity_groups" "existing_security_admin_group" {
   compartment_id = var.tenancy_ocid
   filter {
     name   = "name"
     values = [var.existing_security_admin_group_name]
   }
+}
+
+data "oci_identity_group" "existing_network_admin_group" {
+  group_id = length(trimspace(var.existing_network_admin_group_name)) > 0 ? var.existing_network_admin_group_name : "nogroup"
 }
 
 data "oci_identity_groups" "existing_network_admin_group" {
@@ -43,12 +59,20 @@ data "oci_identity_groups" "existing_network_admin_group" {
   }
 }
 
+data "oci_identity_group" "existing_appdev_admin_group" {
+  group_id = length(trimspace(var.existing_appdev_admin_group_name)) > 0 ? var.existing_appdev_admin_group_name : "nogroup"
+}
+
 data "oci_identity_groups" "existing_appdev_admin_group" {
   compartment_id = var.tenancy_ocid
   filter {
     name   = "name"
     values = [var.existing_appdev_admin_group_name]
   }
+}
+
+data "oci_identity_group" "existing_database_admin_group" {
+  group_id = length(trimspace(var.existing_database_admin_group_name)) > 0 ? var.existing_database_admin_group_name : "nogroup"
 }
 
 data "oci_identity_groups" "existing_database_admin_group" {
@@ -59,12 +83,20 @@ data "oci_identity_groups" "existing_database_admin_group" {
   }
 }
 
+data "oci_identity_group" "existing_auditor_group" {
+  group_id = length(trimspace(var.existing_auditor_group_name)) > 0 ? var.existing_auditor_group_name : "nogroup"
+}
+
 data "oci_identity_groups" "existing_auditor_group" {
   compartment_id = var.tenancy_ocid
   filter {
     name   = "name"
     values = [var.existing_auditor_group_name]
   }
+}
+
+data "oci_identity_group" "existing_announcement_reader_group" {
+  group_id = length(trimspace(var.existing_announcement_reader_group_name)) > 0 ? var.existing_announcement_reader_group_name : "nogroup"
 }
 
 data "oci_identity_groups" "existing_announcement_reader_group" {
@@ -75,12 +107,20 @@ data "oci_identity_groups" "existing_announcement_reader_group" {
   }
 }
 
+data "oci_identity_group" "existing_exainfra_admin_group" {
+  group_id = length(trimspace(var.existing_exainfra_admin_group_name)) > 0 ? var.existing_exainfra_admin_group_name : "nogroup"
+}
+
 data "oci_identity_groups" "existing_exainfra_admin_group" {
   compartment_id = var.tenancy_ocid
   filter {
     name   = "name"
     values = [var.existing_exainfra_admin_group_name]
   }
+}
+
+data "oci_identity_group" "existing_cost_admin_group" {
+  group_id = length(trimspace(var.existing_cost_admin_group_name)) > 0 ? var.existing_cost_admin_group_name : "nogroup"
 }
 
 data "oci_identity_groups" "existing_cost_admin_group" {

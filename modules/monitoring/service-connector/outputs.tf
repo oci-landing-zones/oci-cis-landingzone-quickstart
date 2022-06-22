11ac1214fec7a732271284b0dd77d15f626afebf
Andre Correa <andre.correa@oracle.com>
2022-06-22 11:00:28 -0300

    feat: new service connector hub module

--- modules/monitoring/service-connector/outputs.tf
+++ modules/monitoring/service-connector/outputs.tf
@@ -2,6 +2,16 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 output "service_connector" {
-  description = "service connector information"
-  value       = oci_sch_service_connector.this
+  description = "Managed Service Connector information"
+  value       = oci_sch_service_connector.logging
+}
+
+output "service_connector_target_bucket" {
+  description = "Managed Object Storage Bucket used as Service Connector target"
+  value       = oci_objectstorage_bucket.sch != null ? oci_objectstorage_bucket.sch : ""
+}
+
+output "service_connector_target_stream" {
+  description = "Managed Stream used as Service Connector target"
+  value       = oci_streaming_stream.sch != null ? oci_streaming_stream.sch : ""
 }
--- modules/monitoring/service-connector/variables.tf
+++ modules/monitoring/service-connector/variables.tf
@@ -1,32 +1,178 @@
-# Copyright (c) 2020 Oracle and/or its affiliates.
+# Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-variable "service_connector" {
-    description = "Details of the Service Connector to be created"
-    type = object ({
-        compartment_id  = string,
-	defined_tags = map(string),
-	freeform_tags = map(string),
-        service_connector_display_name = string,
-        service_connector_source_kind  = string,
-        service_connector_state = string,
-        log_sources = list(object({
-            compartment_id = string,
-            log_group_id   = string,
-            log_id         = string
-        })),
-        target = object({
-            target_kind             = string,
-            compartment_id             = string,
-            object_store_details = object({
-                namespace = string,
-                bucket_name = string,
-                object_name_prefix = string,
-                batch_rollover_size_in_mbs = number,
-                batch_rollover_time_in_ms  = number
-            }),
-            stream_id = string,
-            function_id = string
-        })
-    })
+#---------------------------------------------------------------
+#--- Service Connector variables -------------------------------
+#---------------------------------------------------------------
+variable "tenancy_ocid" {
+    description = "The tenancy ocid."
+    type = string
+}
+
+variable "service_label" {
+    description = "The service label."
+    type = string
+}
+
+variable "compartment_id" {
+    description = "The compartment ocid where to create the Service Connector."
+    type = string
+}
+
+variable "display_name" {
+    description = "The Service Connector display name."
+    type = string
+    default = "service-connector"
+}
+
+variable "enable_service_connector" {
+    description = "Whether the Service Connector should be enabled."
+    type = bool
+    default = false
+}
+
+variable "defined_tags" {
+    description = "The Service Connector defined tags."
+    type = map(string)
+    default = null
+}
+
+variable "freeform_tags" {
+    description = "The Service Connector freeform tags."
+    type = map(string)
+    default = null
+}
+
+#---------------------------------------------------------------
+#--- Sources variables ------------------------------------------
+#---------------------------------------------------------------
+variable "logs_sources" {
+    description = "The Service Connector logs sources."
+    type = list(object({
+        compartment_id = string,
+        log_group_id   = string,
+        log_id         = string
+    }))
+}
+
+#---------------------------------------------------------------
+#--- Target variables ------------------------------------------
+#---------------------------------------------------------------
+variable "target_kind" {
+    description = "The target kind."
+    type = string
+    default = "objectstorage"
+    validation {
+        condition     = contains(["objectstorage", "streaming", "functions"], var.target_kind)
+        error_message = "Validation failed for target_kind: valid values are objectstorage, streaming or functions."
+    }
+}
+
+variable "target_compartment_id" {
+    description = "The target compartment ocid."
+    type = string
+}
+
+variable "target_bucket_name" {
+    description = "The target Object Storage bucket name to be created."
+    type = string
+    default = "service-connector-bucket"
+}
+
+variable "target_object_name_prefix" {
+    description = "The target Object Storage object name prefix."
+    type = string
+    default = "sch"
+}
+
+variable "target_bucket_kms_key_id" {
+    description = "The KMS key ocid used to encrypt the target Object Storage bucket."
+    type = string
+}
+
+variable "target_object_store_batch_rollover_size_in_mbs" {
+    description = "The batch rollover size in megabytes."
+    type = number
+    default = 100
+}
+
+variable "target_object_store_batch_rollover_time_in_ms" {
+    description = "The batch rollover time in milliseconds."
+    type = number
+    default = 420000
+}
+
+variable "target_bucket_defined_tags" {
+    description = "The Service Connector target bucket defined tags."
+    type = map(string)
+    default = null
+}
+
+variable "target_bucket_freeform_tags" {
+    description = "The Service Connector target bucket freeform tags."
+    type = map(string)
+    default = null
+}
+
+variable "target_stream" {
+    description = "The target stream name or ocid. If a name is given, a new stream is created. If an ocid is given, the existing stream is used."
+    type = string
+    default = "service-connector-stream"
+}
+
+variable "target_stream_partitions" {
+    description = "The number of partitions in the target stream. Applicable if target_stream is not an ocid."
+    type = number
+    default = 1
+}
+
+variable "target_stream_retention_in_hours" {
+    description = "The retention period of the target stream, in hours. Applicable if target_stream is not an ocid."
+    type = number
+    default = 24
+}
+
+variable "target_stream_defined_tags" {
+    description = "The Service Connector target stream defined tags."
+    type = map(string)
+    default = null
+}
+
+variable "target_stream_freeform_tags" {
+    description = "The Service Connector target stream freeform tags."
+    type = map(string)
+    default = null
+}
+
+variable "target_function_id" {
+    description = "The target function ocid."
+    type = string
+    default = null
+}
+
+#---------------------------------------------------------------
+#--- Policy variables ------------------------------------------
+#---------------------------------------------------------------
+variable "policy_compartment_id" {
+    description = "The Service Connector policy compartment ocid"
+    type = string
+    default = null
+}
+
+variable "target_policy_name" {
+    description = "The Service Connector target policy name"
+    type = string
+    default = "service-connector-target-policy"
+}
+
+variable "policy_defined_tags" {
+    description = "The Service Connector policy defined tags"
+    type = map(string)
+    default = null
+}
+
+variable "policy_freeform_tags" {
+    description = "The Service Connector policy freeform tags"
+    type = map(string)
+    default = null
 }

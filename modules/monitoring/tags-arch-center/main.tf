4a89993c056cc071173a0774bb9fa9822c0f2f5e
Andre Correa <andre.correa@oracle.com>
2022-06-23 11:35:12 -0300

    fix: oracle/oci provider added

--- modules/monitoring/tags-arch-center/main.tf
+++ modules/monitoring/tags-arch-center/main.tf
@@ -1,6 +1,14 @@
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+    }
+  }
+}
+
 resource "oci_identity_tag_namespace" "arch_center" {
   compartment_id = var.tenancy_ocid
   description    = "CIS Landing Zone tag namespace for OCI Architecture Center."

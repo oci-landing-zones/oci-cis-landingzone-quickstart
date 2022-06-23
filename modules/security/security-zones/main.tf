506b62222482834e90fa900100916c05461afebf
Josh Hammer <josh@halimer.com>
2022-06-23 16:56:25 -0400

    feature: stub of security zone module

---
+++ modules/security/security-zones/main.tf
@@ -0,0 +1,7 @@
+# Copyright (c) 2022 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+locals ={
+    cis_1_2_L2_policies = []
+    cis_1_2_L1_policies = []
+}

52974e2bdf580eb66ff83018b00caf3b97e07976
Josh Hammer <josh@halimer.com>
2022-07-08 10:59:27 -0400

    feat: converting Security Zone module to take map object

--- config/security_zones.tf
+++ config/security_zones.tf
@@ -1,20 +1,50 @@
-# Copyright (c) 2021 Oracle and/or its affiliates.
+# Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 
 
-module "lz_sch_vcnFlowLogs_bucket" {
-    depends_on = [ null_resource.slow_down_buckets ]
-    count = (var.create_service_connector_vcnFlowLogs  == true && lower(var.service_connector_vcnFlowLogs_target) == "objectstorage") ? 1 : 0
-    source       = "../modules/object-storage/bucket"
-    kms_key_id   = module.lz_keys.keys[local.oss_key_name].id
-    buckets = { 
-        (local.sch_vcnFlowLogs_bucket_name) = {
-            compartment_id = local.security_compartment_id #module.lz_compartments.compartments[local.security_compartment.key].id
-            name = local.sch_vcnFlowLogs_bucket_name
-            namespace = data.oci_objectstorage_namespace.this.namespace
-            defined_tags = local.service_connector_defined_tags
-            freeform_tags = local.service_connector_freeform_tags
+# module "lz_security_zones" {
+#     # depends_on = [ null_resource.slow_down_buckets ]
+#     # count = (var.create_service_connector_vcnFlowLogs  == true && lower(var.service_connector_vcnFlowLogs_target) == "objectstorage") ? 1 : 0
+    
+#     source       = "../modules/security/security-zones"
+#     providers = { oci = oci.home }
+
+#     tenancy_ocid = var.tenancy_ocid
+#     service_label = var.service_label
+#     compartment_id = local.enclosing_compartment_id
+# }
+
+module "lz_security_zones" {
+    # depends_on = [ null_resource.slow_down_buckets ]
+    # count = (var.create_service_connector_vcnFlowLogs  == true && lower(var.service_connector_vcnFlowLogs_target) == "objectstorage") ? 1 : 0
+    
+    source       = "../modules/security/security-zones"
+    providers = { oci = oci.home }
+    security_zones = {
+        ("key") = {
+            name                = "Name"
+            tenancy_ocid        = var.tenancy_ocid
+            service_label       = var.service_label
+            description         = null
+            security_policies   = null
+            cis_level           = "2"
+            defined_tags        = local.security_zones_defined_tags
+            freeform_tags       = local.security_zones_freeform_tags
         }
     }
+
+}
+locals {
+
+  all_security_zones_defined_tags = {}
+  all_security_zones_freeform_tags = {}
+
+
+  ### DON'T TOUCH THESE ###
+  default_security_zones_defined_tags = null
+  default_security_zones_freeform_tags = local.landing_zone_tags
+
+  security_zones_defined_tags = length(local.all_security_zones_defined_tags) > 0 ? local.all_security_zones_defined_tags : local.default_security_zones_defined_tags
+  security_zones_freeform_tags = length(local.all_security_zones_freeform_tags) > 0 ? merge(local.all_security_zones_freeform_tags, local.default_security_zones_freeform_tags) : local.default_security_zones_freeform_tags
 }
--- modules/security/security-zones/main.tf
+++ modules/security/security-zones/main.tf
@@ -1,19 +1,42 @@
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-locals ={
+terraform {
+  required_providers {
+    oci = {
+      source = "hashicorp/oci"
+    }
+  }
+}
+
+locals {
+    
+    
     cis_1_2_L2 = [
         "ocid1.securityzonessecuritypolicy.oc1..aaaaaaaa5ocyo7jqjzgjenvccch46buhpaaofplzxlp3xbxfcdwwk2tyrwqa",
         "ocid1.securityzonessecuritypolicy.oc1..aaaaaaaauoi2xnbusvfd4yffdjaaazk64gndp4flumaw3r7vedwndqd6vmrq"
                         ]
     cis_1_2_L1 = [
-        "ocid1.securityzonessecuritypolicy.oc1..aaaaaaaa7pgtjyod3pze6wuylgmts6ensywmeplabsxqq2bk4ighps4fqq4a",
+        "ocid1.securityzonessecuritypolicy.oc1..aaaaaaaa7pgtjyod3pze6wuylgmts6ensywmeplabsxqq2bk4ighps4fqq4a", 
         "ocid1.securityzonessecuritypolicy.oc1..aaaaaaaaxxs63ulmtcnxqmcvy6eaozh5jdtiaa2bk7wll5bbdsbnmmoczp5a",
         "ocid1.securityzonessecuritypolicy.oc1..aaaaaaaaqmq4jqcxqbjj5cjzb7t5ira66dctyypq2m2o4psxmx6atp45lyda",
         "ocid1.securityzonessecuritypolicy.oc1..aaaaaaaaff6n52aojbgdg46jpm3kn7nizmh6iwvr7myez7svtfxsfs7irigq",
     ]
 
-    sz_policies = var.cis_level == "2" ? setunion(cis_1_2_L2,cis_1_2_L1,var.security_policies) : setunion(cis_1_2_L1,var.security_policies)
+    sz_policies = var.cis_level == "2" ? setunion(local.cis_1_2_L2,local.cis_1_2_L1,var.security_policies) : setunion(local.cis_1_2_L1,var.security_policies)
+
+  security_zones = {
+    for k, v in var.security_zones : k => {
+      name = v.name
+      tenancy_ocid = v.tenancy_ocid
+      service_label = v.service_label
+      description   = v.description
+      security_policies = coalesce(v.security_policies,[])
+      cis_level         = coalesce(v.cis_level,"2")
+      defined_tags      = v.defined_tags
+      freeform_tags     = v.freeform_tags
+    }
+  }
 
 }
 
@@ -21,23 +44,24 @@ locals ={
 resource "oci_cloud_guard_security_recipe" "this" {
     #Required
     compartment_id = var.compartment_id
-    display_name = var.security_recipe_display_name
+    display_name = var.description
     security_policies = local.sz_policies
 
     #Optional
     defined_tags = var.defined_tags
     description = "${var.description} recipe."
-    freeform_tags = var.defined_tags
+    freeform_tags = var.freeform_tags
 }
 
 resource "oci_cloud_guard_security_zone" "this" {
     #Required
+    # depends_on = [ oci_cloud_guard_security_recipe]
     compartment_id = var.compartment_id
-    display_name = var.security_zone_display_name
-    security_zone_recipe_id = oci_cloud_guard_security_zone_recipe.test_security_zone_recipe.id
+    display_name = var.description
+    security_zone_recipe_id = oci_cloud_guard_security_recipe.this.id
 
     #Optional
     defined_tags = var.defined_tags
     description = "${var.description}."
-    freeform_tags = var.defined_tags
+    freeform_tags = var.freeform_tags
 }

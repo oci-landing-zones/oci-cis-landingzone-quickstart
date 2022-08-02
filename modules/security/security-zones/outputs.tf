c9d8a21f8b93d64f6c918b20d3204d1c7dd0078e
Josh Hammer <josh@halimer.com>
2022-08-02 18:12:12 -0400

    feat: Added security zones module with CIS compliance

--- modules/security/security-zones/outputs.tf
+++ modules/security/security-zones/outputs.tf
@@ -1,24 +1,12 @@
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-# output "security_zone" {
-#   description = "OCI Security Zone"
-#   value       = oci_cloud_guard_security_zone.these
-# }
-
-# output "security_zone_recipe" {
-#   description = "OCI Security Zone Recipe"
-#   value       = oci_cloud_guard_security_recipe.this
-# }
-
-
-
-output "security_zone" {
+output "security_zone_recipes" {
   description = "The seccurity zones, indexed by keys."
-  value = {for k, v in local.security_zones : k => oci_cloud_guard_security_zone.these[k]}
+  value = {for v in oci_cloud_guard_security_zone.these : v.display_name => v}
 } 
 
-output "security_zone_recipe" {
+output "security_zones" {
   description = "The seccurity zones, indexed by keys."
-  value = {for k, v in local.security_zones : k => oci_cloud_guard_security_recipe.these[k]}
+  value = {for v in oci_cloud_guard_security_zone.these : v.display_name => v}
 } 

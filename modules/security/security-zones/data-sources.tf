767faedb6d6bbead7003af92399ca14977ea6985
Josh Hammer <josh@halimer.com>
2022-08-04 16:58:11 -0400

    fix: clean up

--- modules/security/security-zones/data-sources.tf
+++ modules/security/security-zones/data-sources.tf
@@ -13,22 +13,22 @@ data "oci_identity_compartment" "enclosing_compartment" {
 
 data "oci_identity_compartment" "appdev_compartment" {
   count = var.appdev_compartment_id != null ? 1 : 0
-  id    = var.appdev_compartment_id != null ? var.appdev_compartment_id : null
+  id    = var.appdev_compartment_id
 }
 
 data "oci_identity_compartment" "database_compartment" {
   count = var.database_compartment_id != null ? 1 : 0
-  id    = var.database_compartment_id != null ? var.database_compartment_id : null
+  id    = var.database_compartment_id
 }
 
 data "oci_identity_compartment" "network_compartment" {
   count = var.network_compartment_id != null ? 1 : 0
-  id    = var.network_compartment_id != null ? var.network_compartment_id : null
+  id    = var.network_compartment_id
 }
 
 data "oci_identity_compartment" "security_compartment" {
   count = var.security_compartment_id != null ? 1 : 0
-  id    = var.security_compartment_id != null ? var.security_compartment_id : null
+  id    = var.security_compartment_id
 }
 
 

35f8b9d95677be542bad1c902ef004e2318a2265
Andre Correa <andre.correa@oracle.com>
2022-08-11 10:51:37 -0300

    feat: vss module initial refactoring

--- modules/security/vss/main.tf
+++ modules/security/vss/main.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Oracle and/or its affiliates.
+# Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 terraform {
@@ -9,13 +9,82 @@ terraform {
   }
 }
 
+locals {
+
+  #-- Three variables below are to keep module backwards compatibility, as the "scan-target" suffix has been used 
+  #-- as part of the key name to index the map variable for the creation of target resources.
+  target_suffix = "scan-target"
+  compat_scan_targets = {for k, v in var.vss_targets : "${k}-${local.target_suffix}" => v}
+  compat_scan_target_names = [for t in var.vss_target_names : "${t}-${local.target_suffix}"]
+
+  default_recipe_name = "default-scan-recipe"
+  recipe_name = var.vss_recipe_name != null ? var.vss_recipe_name : (var.name_prefix != null ? "${var.name_prefix}-${local.default_recipe_name}" : local.default_recipe_name)
+
+  scan_recipes = {
+    (local.recipe_name) = {
+      compartment_id  = var.compartment_id
+      name            = local.recipe_name
+      port_scan_level = "STANDARD"
+      # Valid values: STANDARD, LIGHT, NONE 
+      # STANDARD checks the 1000 most common port numbers.
+      # LIGHT checks the 100 most common port numbers.
+      # NONE does not check for open ports.
+      schedule_type        = upper(var.vss_scan_schedule)
+      schedule_day_of_week = upper(var.vss_scan_day)
+      agent_scan_level     = "STANDARD"
+      # Valid values: STANDARD, NONE
+      # STANDARD enables agent-based scanning.
+      # NONE disables agent-based scanning and moots all subsequent agent related attributes. 
+      agent_configuration_vendor = "OCI"
+      # Valid values: OCI
+      agent_cis_benchmark_settings_scan_level = "MEDIUM"
+      # Valid values: STRICT, MEDIUM, LIGHTWEIGHT, NONE
+      # STRICT: If more than 20% of the CIS benchmarks fail, then the target is assigned a risk level of Critical.
+      # MEDIUM: If more than 40% of the CIS benchmarks fail, then the target is assigned a risk level of High. 
+      # LIGHTWEIGHT: If more than 80% of the CIS benchmarks fail, then the target is assigned a risk level of High.
+      # NONE: disables cis benchmark scanning.
+      defined_tags = var.defined_tags
+      freeform_tags = var.freeform_tags
+    }
+  }
+
+  scan_targets = { for k, v in local.compat_scan_targets : k => {
+    compartment_id = var.compartment_id
+    name = "${v.target_compartment_name}-${local.target_suffix}"
+    description = "CIS Landing Zone ${v.target_compartment_name} compartment scanning target."
+    scan_recipe_name = local.recipe_name
+    target_compartment_id = v.target_compartment_id
+    defined_tags = var.defined_tags
+    freeform_tags = var.freeform_tags
+  } }
+
+  #-- VSS policy variables
+  default_policy_name = "vss-policy"
+  policy_name = var.vss_policy_name != null ? var.vss_policy_name : (var.name_prefix != null ? "${var.name_prefix}-${local.default_policy_name}" : local.default_policy_name)
+  vss_grants = [
+    "allow service vulnerability-scanning-service to manage instances in tenancy",
+    "allow service vulnerability-scanning-service to read compartments in tenancy",
+    "allow service vulnerability-scanning-service to read repos in tenancy",
+    "allow service vulnerability-scanning-service to read vnics in tenancy",
+    "allow service vulnerability-scanning-service to read vnic-attachments in tenancy"
+  ]
+
+  lz_recipe_key_map = {
+    "LZ-RECIPE" : local.recipe_name
+  }
+
+}
+
+#---------------------------------------------------------------------------------------
+#-- Default recipes resources.
+#---------------------------------------------------------------------------------------
 resource "oci_vulnerability_scanning_host_scan_recipe" "these" {
   lifecycle {
       create_before_destroy = true
   }  
-  for_each = var.scan_recipes
+  for_each = var.vss_create ? local.scan_recipes : {}
     compartment_id = each.value.compartment_id
-    display_name = each.key 
+    display_name = each.value.name 
     #Required
     port_settings {
         scan_level = each.value.port_scan_level
@@ -44,15 +113,90 @@ resource "oci_vulnerability_scanning_host_scan_recipe" "these" {
     freeform_tags = each.value.freeform_tags
 }
 
-resource "oci_vulnerability_scanning_host_scan_target" "these" {
-  for_each = var.scan_targets
+#---------------------------------------------------------------------------------------
+#-- Default targets resources.
+#-- We could have looped through local.scan_targets directly, but terraform plan
+#-- errors out with the following message:
+#-- "local.scan_targets is a map of object, known only after apply
+#-- The "for_each" value depends on resource attributes that cannot be determined
+#-- until apply, so Terraform cannot predict how many instances will be created.
+#-- To work around this, use the -target argument to first apply only the
+#-- resources that the for_each depends on."
+#-- Hence the approach of looping through a list of target names (var.scan_target_names) 
+#-- that MUST contain the same keys used by local.scan_targets. 
+#---------------------------------------------------------------------------------------
+ resource "oci_vulnerability_scanning_host_scan_target" "these" {
+  for_each = var.vss_create ? (length(var.vss_custom_targets) == 0 ? toset(local.compat_scan_target_names) : toset([])) : toset([])
+    compartment_id        = local.scan_targets[each.key].compartment_id
+    display_name          = local.scan_targets[each.key].name
+    description           = local.scan_targets[each.key].description
+    host_scan_recipe_id   = oci_vulnerability_scanning_host_scan_recipe.these[local.recipe_name].id
+    target_compartment_id = local.scan_targets[each.key].target_compartment_id
+    defined_tags          = local.scan_targets[each.key].defined_tags
+    freeform_tags         = local.scan_targets[each.key].freeform_tags
+ }
+
+  #----------------------------------------------------------------------------
+  #-- VSS policy resource.
+  #----------------------------------------------------------------------------
+  resource "oci_identity_policy" "vss" {
+    count = var.vss_create ? 1 : 0
+      name           = local.policy_name
+      description    = "CIS Landing Zone policy for VSS (Vulnerability Scanning Service)."
+      compartment_id = var.tenancy_id
+      statements     = local.vss_grants
+      defined_tags   = var.defined_tags
+      freeform_tags  = var.freeform_tags
+}
+
+ #-----------------------------------------------------------------------------
+ #-- Custom recipes resources, overriding the default recipes.
+ #----------------------------------------------------------------------------- 
+ resource "oci_vulnerability_scanning_host_scan_recipe" "custom" {
+  lifecycle {
+      create_before_destroy = true
+  }  
+  for_each = var.vss_create ? var.vss_custom_recipes : {}
+    compartment_id = each.value.compartment_id
+    display_name = each.value.name
+    #Required
+    port_settings {
+        scan_level = each.value.port_scan_level
+    }
+    #Required
+    schedule {
+        #Required
+        type = each.value.schedule_type
+        #Optional
+        day_of_week = each.value.schedule_day_of_week
+    } 
+    #Required
+    agent_settings {
+        #Required
+        scan_level = each.value.agent_scan_level
+        #Optional
+        agent_configuration {
+            vendor = each.value.agent_configuration_vendor
+            cis_benchmark_settings {
+                scan_level = each.value.agent_cis_benchmark_settings_scan_level
+            }
+        }
+    }
+    #Optional
+    defined_tags = each.value.defined_tags
+    freeform_tags = each.value.freeform_tags
+}
+
+ #-----------------------------------------------------------------------------
+ #-- Custom targets resources, overriding the default targets.
+ #-----------------------------------------------------------------------------
+ resource "oci_vulnerability_scanning_host_scan_target" "custom" {
+  for_each = var.vss_create ? var.vss_custom_targets : {}
     compartment_id        = each.value.compartment_id
-    display_name          = each.key
+    display_name          = each.value.name
     description           = each.value.description
-    host_scan_recipe_id   = oci_vulnerability_scanning_host_scan_recipe.these[each.value.scan_recipe_name].id
+    host_scan_recipe_id   = upper(substr(each.value.recipe_key,0,3)) == "LZ-" ? oci_vulnerability_scanning_host_scan_recipe.these[local.lz_recipe_key_map[upper(each.value.recipe_key)]].id : oci_vulnerability_scanning_host_scan_recipe.custom[each.value.recipe_key].id
     target_compartment_id = each.value.target_compartment_id
     defined_tags          = each.value.defined_tags
     freeform_tags         = each.value.freeform_tags
-    
-    
- }
+ }  
--- modules/security/vss/variables.tf
+++ modules/security/vss/variables.tf
@@ -1,24 +1,112 @@
-variable "scan_recipes" {
+# Copyright (c) 2022 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+variable "tenancy_id" {
+  description = "The tenancy ocid."
+  type = string
+}
+
+variable "compartment_id" {
+  description = "The compartment ocid where VSS recipes and targets are created."
+  type = string
+}
+
+variable "name_prefix" {
+  description = "A prefix used when naming resources created by this module."
+  type = string
+  default = null
+}
+
+variable "vss_create" {
+  description = "Whether or not VSS resources (recipes, targets and policies) are to be created."
+  type = bool
+  default = true
+}
+
+variable "vss_recipe_name" {
+  description = "The recipe name. Use it to override the default one, that is either <name-prefix>-default-scan-recipe or default-scan-recipe."
+  type = string
+  default = null
+}
+
+variable "vss_scan_schedule" {
+  description = "The scan schedule for the VSS recipe, if enabled. Valid values are WEEKLY or DAILY (case insensitive)."
+  type = string
+  default     = "WEEKLY"
+  validation {
+    condition     = contains(["WEEKLY", "DAILY"], upper(var.vss_scan_schedule))
+    error_message = "Validation failed for vss_scan_schedule: valid values are WEEKLY or DAILY (case insensitive)."
+  }
+}
+
+variable "vss_scan_day" {
+  description = "The week day for the VSS recipe, if enabled. Only applies if vss_scan_schedule is WEEKLY (case insensitive)."
+  type        = string
+  default     = "SUNDAY"
+  validation {
+    condition     = contains(["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"], upper(var.vss_scan_day))
+    error_message = "Validation failed for vss_scan_day: valid values are SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY (case insensitive)."
+  }
+}
+
+variable "vss_target_names" {
+  description = "A list with the VSS target names."
+  type = list(string)
+}
+
+variable "vss_targets" {
+  description = "The VSS targets. The map indexes MUST match the values in vss_target_names."
   type = map(object({
-    compartment_id                                = string,
-    agent_scan_level                              = string,
-    agent_configuration_vendor                    = string,
-    agent_cis_benchmark_settings_scan_level       = string,
-    port_scan_level                               = string,
-    schedule_type                                 = string,
-    schedule_day_of_week                          = string,
-    defined_tags                                  = map(string)
-    freeform_tags                                 = map(string)
+    target_compartment_id = string
+    target_compartment_name = string
   }))
 }
 
-variable "scan_targets" {
+variable "vss_policy_name" {
+  description = "The VSS policy name. Use it to override the default policy name, which is either <name-prefix>-vss-policy or vss-policy."
+  type = string
+  default = null
+}
+
+variable "defined_tags" {
+  description = "Any defined tags to apply on the VSS resources." 
+  type = map(string)
+  default = {}
+}
+
+variable "freeform_tags" {
+  description = "Any freeform tags to apply on the VSS resources."
+  type = map(string)
+  default = {}
+}
+
+variable "vss_custom_recipes" {
+  description = "VSS custom recipes. Use it to override the default recipe."
+  type = map(object({
+    compartment_id                          = string,
+    name                                    = string,
+    agent_scan_level                        = string,
+    agent_configuration_vendor              = string,
+    agent_cis_benchmark_settings_scan_level = string,
+    port_scan_level                         = string,
+    schedule_type                           = string,
+    schedule_day_of_week                    = string,
+    defined_tags                            = map(string),
+    freeform_tags                           = map(string)
+  }))
+  default = {}
+}
+
+variable "vss_custom_targets" {
+  description = "VSS custom targets. Use it to override the default targets. For recipe_key, pass the corresponding key in vss_custom_recipes."
   type = map(object({
     compartment_id        = string,
+    name                  = string,
     description           = string,
-    scan_recipe_name      = string,
+    recipe_key            = string,
     target_compartment_id = string,
     defined_tags          = map(string),
     freeform_tags         = map(string)
   }))
+  default = {}
 }

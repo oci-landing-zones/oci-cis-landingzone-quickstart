36f0f6428d4775fb5d6f35828f244b29e2b2637c
Andre Correa <andre.correa@oracle.com>
2022-08-19 17:14:09 -0300

    feat: module description, vars defaults updated

--- modules/security/vaults/main.tf
+++ modules/security/vaults/main.tf
@@ -1,4 +1,10 @@
-# Copyright (c) 2020 Oracle and/or its affiliates.
+/**
+ * ## CIS OCI Landing Zone KMS Vaults Module.
+ *
+ * This module manages a single OCI KMS vault resource defined by var.name and var.type in compartment var.compartment_id.
+ */
+
+# Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 terraform {
@@ -10,9 +16,9 @@ terraform {
 }
 
 resource "oci_kms_vault" "this" {
-    compartment_id = var.compartment_id
-    display_name   = var.vault_name
-    vault_type     = var.vault_type
-    defined_tags   = var.defined_tags
-    freeform_tags  = var.freeform_tags
+  compartment_id = var.compartment_id
+  display_name   = var.name
+  vault_type     = var.type
+  defined_tags   = var.defined_tags
+  freeform_tags  = var.freeform_tags
 }

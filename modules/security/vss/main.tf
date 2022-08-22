dfbd07d4e54305a92ad99d8d3a2e56979c4bb84b
Andre Correa <andre.correa@oracle.com>
2022-08-22 18:36:32 -0300

    feat: file scan support, recipe vars externalized

--- modules/security/vss/main.tf
+++ modules/security/vss/main.tf
@@ -1,3 +1,10 @@
+/** 
+ * ## CIS OCI Landing Zone Vulnerability Scanning Service (VSS) Module
+ *
+ * This module manages one single VSS recipe, multiple VSS targets and one IAM policy for VSS. 
+ * The recipe is assigned to all provided targets.
+ */
+
 # Copyright (c) 2022 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
@@ -17,27 +24,24 @@ locals {
   compat_scan_targets = {for k, v in var.vss_targets : "${k}-${local.target_suffix}" => v}
   compat_scan_target_names = [for t in var.vss_target_names : "${t}-${local.target_suffix}"]
 
-  default_recipe_name = "default-scan-recipe"
-  recipe_name = var.vss_recipe_name != null ? var.vss_recipe_name : (var.name_prefix != null ? "${var.name_prefix}-${local.default_recipe_name}" : local.default_recipe_name)
-
   scan_recipes = {
-    (local.recipe_name) = {
+    (var.vss_recipe_name) = {
       compartment_id  = var.compartment_id
-      name            = local.recipe_name
-      port_scan_level = "STANDARD"
+      name            = var.vss_recipe_name
+      port_scan_level = upper(var.vss_port_scan_level)
       # Valid values: STANDARD, LIGHT, NONE 
       # STANDARD checks the 1000 most common port numbers.
       # LIGHT checks the 100 most common port numbers.
       # NONE does not check for open ports.
       schedule_type        = upper(var.vss_scan_schedule)
       schedule_day_of_week = upper(var.vss_scan_day)
-      agent_scan_level     = "STANDARD"
+      agent_scan_level     = upper(var.vss_agent_scan_level)
       # Valid values: STANDARD, NONE
       # STANDARD enables agent-based scanning.
       # NONE disables agent-based scanning and moots all subsequent agent related attributes. 
       agent_configuration_vendor = "OCI"
       # Valid values: OCI
-      agent_cis_benchmark_settings_scan_level = "MEDIUM"
+      agent_cis_benchmark_settings_scan_level = upper(var.vss_agent_cis_benchmark_settings_scan_level)
       # Valid values: STRICT, MEDIUM, LIGHTWEIGHT, NONE
       # STRICT: If more than 20% of the CIS benchmarks fail, then the target is assigned a risk level of Critical.
       # MEDIUM: If more than 40% of the CIS benchmarks fail, then the target is assigned a risk level of High. 
@@ -52,15 +56,12 @@ locals {
     compartment_id = var.compartment_id
     name = "${v.target_compartment_name}-${local.target_suffix}"
     description = "CIS Landing Zone ${v.target_compartment_name} compartment scanning target."
-    scan_recipe_name = local.recipe_name
+    scan_recipe_name = var.vss_recipe_name
     target_compartment_id = v.target_compartment_id
     defined_tags = var.defined_tags
     freeform_tags = var.freeform_tags
   } }
 
-  #-- VSS policy variables
-  default_policy_name = "vss-policy"
-  policy_name = var.vss_policy_name != null ? var.vss_policy_name : (var.name_prefix != null ? "${var.name_prefix}-${local.default_policy_name}" : local.default_policy_name)
   vss_grants = [
     "allow service vulnerability-scanning-service to manage instances in tenancy",
     "allow service vulnerability-scanning-service to read compartments in tenancy",
@@ -69,12 +70,9 @@ locals {
     "allow service vulnerability-scanning-service to read vnic-attachments in tenancy"
   ]
 
-  #-- Map used to retrieve the actual index for Landing Zone default recipe. 
-  #-- Used when overriding VSS targets.
-  lz_recipe_key_map = {
-    "LZ-RECIPE" : local.recipe_name
-  }
-
+  #-- Supported file scan recurrences by VSS: BI-WEEKLY, MONTHLY
+  #-- If overall scan schedule is WEEKLY, file scan uses the same scan day. If overall scan sechdule is DAILY, we set file scan day to Sundays (WKST=SU).
+  file_scan_recurrence = upper(var.vss_scan_schedule) == "WEEKLY" ? "FREQ=WEEKLY;WKST=${substr(upper(var.vss_scan_day),0,2)};INTERVAL=2" : "FREQ=WEEKLY;WKST=SU;INTERVAL=2"
 }
 
 #---------------------------------------------------------------------------------------
@@ -84,31 +82,41 @@ resource "oci_vulnerability_scanning_host_scan_recipe" "these" {
   lifecycle {
       create_before_destroy = true
   }  
-  for_each = var.vss_create ? local.scan_recipes : {}
+  for_each = local.scan_recipes
     compartment_id = each.value.compartment_id
     display_name = each.value.name 
     #Required
     port_settings {
-        scan_level = each.value.port_scan_level
+      scan_level = each.value.port_scan_level
     }
     #Required
     schedule {
-        #Required
-        type = each.value.schedule_type
-        #Optional
-        day_of_week = each.value.schedule_day_of_week
+      #Required
+      type = each.value.schedule_type
+      #Optional
+      day_of_week = each.value.schedule_day_of_week
     } 
     #Required
     agent_settings {
-        #Required
-        scan_level = each.value.agent_scan_level
-        #Optional
-        agent_configuration {
-            vendor = each.value.agent_configuration_vendor
-            cis_benchmark_settings {
-                scan_level = each.value.agent_cis_benchmark_settings_scan_level
-            }
+      #Required
+      scan_level = each.value.agent_scan_level
+      #Optional
+      agent_configuration {
+        vendor = each.value.agent_configuration_vendor
+        cis_benchmark_settings {
+          scan_level = each.value.agent_scan_level != "NONE" ? each.value.agent_cis_benchmark_settings_scan_level : "NONE"
         }
+      }
+    }
+    #Optional
+    application_settings {
+      #Required
+      application_scan_recurrence = local.file_scan_recurrence
+      folders_to_scan {
+        folder = join(";",var.vss_folders_to_scan)
+        operatingsystem = "LINUX"
+      }
+      is_enabled = var.vss_enable_file_scan
     }
     #Optional
     defined_tags = each.value.defined_tags
@@ -131,79 +139,25 @@ resource "oci_vulnerability_scanning_host_scan_recipe" "these" {
 #-- that MUST contain the same keys used by local.scan_targets. 
 #-- Internal issue reference: 181.
 #---------------------------------------------------------------------------------------
- resource "oci_vulnerability_scanning_host_scan_target" "these" {
-  for_each = var.vss_create ? (length(var.vss_custom_targets) == 0 ? toset(local.compat_scan_target_names) : toset([])) : toset([])
+resource "oci_vulnerability_scanning_host_scan_target" "these" {
+  for_each = toset(local.compat_scan_target_names)
     compartment_id        = local.scan_targets[each.key].compartment_id
     display_name          = local.scan_targets[each.key].name
     description           = local.scan_targets[each.key].description
-    host_scan_recipe_id   = oci_vulnerability_scanning_host_scan_recipe.these[local.recipe_name].id
+    host_scan_recipe_id   = oci_vulnerability_scanning_host_scan_recipe.these[var.vss_recipe_name].id
     target_compartment_id = local.scan_targets[each.key].target_compartment_id
     defined_tags          = local.scan_targets[each.key].defined_tags
     freeform_tags         = local.scan_targets[each.key].freeform_tags
  }
 
-  #----------------------------------------------------------------------------
-  #-- VSS policy resource.
-  #----------------------------------------------------------------------------
-  resource "oci_identity_policy" "vss" {
-    count = var.vss_create ? 1 : 0
-      name           = local.policy_name
-      description    = "CIS Landing Zone policy for VSS (Vulnerability Scanning Service)."
-      compartment_id = var.tenancy_id
-      statements     = local.vss_grants
-      defined_tags   = var.defined_tags
-      freeform_tags  = var.freeform_tags
+#----------------------------------------------------------------------------
+#-- VSS policy resource.
+#----------------------------------------------------------------------------
+resource "oci_identity_policy" "vss" {
+  name           = var.vss_policy_name
+  description    = "CIS Landing Zone policy for VSS (Vulnerability Scanning Service)."
+  compartment_id = var.tenancy_id
+  statements     = local.vss_grants
+  defined_tags   = var.defined_tags
+  freeform_tags  = var.freeform_tags
 }
-
- #-----------------------------------------------------------------------------
- #-- Custom recipes resources. They don't override the default recipe, 
- #-- as the default recipe can be referenced in custom targets.
- #----------------------------------------------------------------------------- 
- resource "oci_vulnerability_scanning_host_scan_recipe" "custom" {
-  lifecycle {
-      create_before_destroy = true
-  }  
-  for_each = var.vss_create ? var.vss_custom_recipes : {}
-    compartment_id = each.value.compartment_id
-    display_name = each.value.name
-    #Required
-    port_settings {
-        scan_level = each.value.port_scan_level
-    }
-    #Required
-    schedule {
-        #Required
-        type = each.value.schedule_type
-        #Optional
-        day_of_week = each.value.schedule_day_of_week
-    } 
-    #Required
-    agent_settings {
-        #Required
-        scan_level = each.value.agent_scan_level
-        #Optional
-        agent_configuration {
-            vendor = each.value.agent_configuration_vendor
-            cis_benchmark_settings {
-                scan_level = each.value.agent_cis_benchmark_settings_scan_level
-            }
-        }
-    }
-    #Optional
-    defined_tags = each.value.defined_tags
-    freeform_tags = each.value.freeform_tags
-}
-
- #-----------------------------------------------------------------------------
- #-- Custom targets resources, overriding the default targets.
- #-----------------------------------------------------------------------------
- resource "oci_vulnerability_scanning_host_scan_target" "custom" {
-  for_each = var.vss_create ? var.vss_custom_targets : {}
-    compartment_id        = each.value.compartment_id
-    display_name          = each.value.name
-    description           = each.value.description
-    host_scan_recipe_id   = upper(substr(each.value.recipe_key,0,3)) == "LZ-" ? oci_vulnerability_scanning_host_scan_recipe.these[local.lz_recipe_key_map[upper(each.value.recipe_key)]].id : oci_vulnerability_scanning_host_scan_recipe.custom[each.value.recipe_key].id
-    target_compartment_id = each.value.target_compartment_id
-    defined_tags          = each.value.defined_tags
-    freeform_tags         = each.value.freeform_tags
- }  

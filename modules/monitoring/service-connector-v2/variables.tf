6858adcd6543f83e5b27b22a3c14a5ea7f8595c4
Andre Correa <andre.correa@oracle.com>
2022-08-23 10:55:36 -0300

    fix: cis_level var defaulted to 1

--- modules/monitoring/service-connector-v2/variables.tf
+++ modules/monitoring/service-connector-v2/variables.tf
@@ -163,7 +163,7 @@ variable "policy_freeform_tags" {
 variable "cis_level" {
   type = string
   description = "The CIS OCI Benchmark profile level for buckets. Level 1 is be practical and prudent. Level 2 is intended for environments where security is more critical than manageability and usability."
-  default = "2"
+  default = "1"
   validation {
     condition     = contains(["1", "2"], var.cis_level)
     error_message = "Validation failed for cis_level: valid values are 1 or 2."

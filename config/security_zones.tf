1d6ea1c475fc9834a9fa5e4ec8e2b25e79f6534f
Andre Correa <andre.correa@oracle.com>
2022-09-12 11:02:11 -0300

    refact: relying on existing logic for retrieving enc cmp

--- config/security_zones.tf
+++ config/security_zones.tf
@@ -7,7 +7,7 @@ locals {
   all_security_zones_freeform_tags = {}
 
   managed_enclosing_target_sz_compartment  = length(module.lz_top_compartment) > 0 ? { "${local.enclosing_compartment.key}-security-zone" = { "sz_compartment_name" : module.lz_top_compartment[0].compartments[local.enclosing_compartment.key].name, "sz_compartment_id" : module.lz_top_compartment[0].compartments[local.enclosing_compartment.key].id } } : {}
-  existing_enclosing_target_sz_compartment = var.existing_enclosing_compartment_ocid != null ? { "${local.enclosing_compartment.key}-security-zone" = { "sz_compartment_name" : data.oci_identity_compartment.enclosing_compartment.name, "sz_compartment_id" : var.existing_enclosing_compartment_ocid } } : {}
+  existing_enclosing_target_sz_compartment = local.enclosing_compartment_id != var.tenancy_ocid ? { "${local.enclosing_compartment.key}-security-zone" = { "sz_compartment_name" : local.enclosing_compartment.name, "sz_compartment_id" : local.enclosing_compartment_id } } : {}
   managed_compartments_sz_compartments     = { for k, v in module.lz_compartments.compartments : "${k}-security-zone" => { "sz_compartment_name" : v.name, "sz_compartment_id" : v.id } }
   auto_security_zone_target_compartments   = length(local.managed_enclosing_target_sz_compartment) > 0 ? local.managed_enclosing_target_sz_compartment : (length(local.existing_enclosing_target_sz_compartment) > 0 ? local.existing_enclosing_target_sz_compartment : local.managed_compartments_sz_compartments)
   custom_security_zone_target_compartments = {}
@@ -15,9 +15,6 @@ locals {
 
 }
 
-data "oci_identity_compartment" "enclosing_compartment" {
-  id = var.existing_enclosing_compartment_ocid != null ? var.existing_enclosing_compartment_ocid : var.tenancy_ocid
-}
 module "lz_security_zones" {
   depends_on = [
     module.lz_compartments

d200572343fea912aaf34bc4a9821d767c9fc205
Andre Correa <andre.correa@oracle.com>
2022-09-28 16:47:18 -0300

    fix: db event names fixed

--- config/mon_notifications.tf
+++ config/mon_notifications.tf
@@ -14,9 +14,9 @@ locals {
     notify_on_compute_changes_rule      = {key:"${var.service_label}-notify-on-compute-changes-rule",       name:"${var.service_label}-notify-on-compute-changes-rule"}
     notify_on_cloudguard_events_rule    = {key:"${var.service_label}-notify-on-cloudguard-events-rule",     name:"${var.service_label}-notify-on-cloudguard-events-rule"}
 
-    exainfra_events = "\"com.oraclecloud.databaseservice.autonomous.exadata.infrastructure.critical\", \"com.oraclecloud.databaseservice.cloudexadatainfrastructure.critical\""
-    default_database_events = "\"com.oraclecloud.databaseservice.autonomous.database.critical\",\"com.oraclecloud.databaseservice.dbsystem.critical\""
-    database_events = var.deploy_exainfra_cmp == true ?  local.default_database_events: "${local.exainfra_events},${local.default_database_events}"
+    default_database_events = ["com.oraclecloud.databaseservice.autonomous.database.critical","com.oraclecloud.databaseservice.dbsystem.critical"]
+    exainfra_events = ["com.oraclecloud.databaseservice.exadatainfrastructure.critical"]
+    database_events = var.deploy_exainfra_cmp == true ?  concat(local.default_database_events,local.exainfra_events) : local.default_database_events
     
     cloudguard_risk_levels = {
     critical = ["CRITICAL"]
@@ -68,13 +68,9 @@ locals {
       description         = "Landing Zone events rule to notify when Cloud Guard problems are Detected, Dismissed or Resolved."
       is_enabled          = true
       condition           = jsonencode(
-           {"eventType":["com.oraclecloud.cloudguard.problemdetected",
-              "com.oraclecloud.cloudguard.problemdismissed",
-              "com.oraclecloud.cloudguard.problemremediated"],
-            "data":{"additionalDetails": {"riskLevel":local.cloudguard_risk_levels[lower(var.cloud_guard_risk_level_threshold)]}
-               }
-            
-            }
+           {"eventType":["com.oraclecloud.cloudguard.problemdetected","com.oraclecloud.cloudguard.problemdismissed","com.oraclecloud.cloudguard.problemremediated"],
+            "data":{"additionalDetails": {"riskLevel":local.cloudguard_risk_levels[lower(var.cloud_guard_risk_level_threshold)]}}
+           }
       )
       actions_action_type = "ONS"
       actions_is_enabled  = true
@@ -167,18 +163,14 @@ locals {
       compartment_id      = local.database_topic.cmp_id       
       description         = "Landing Zone events rule to detect when database resources are created, updated or deleted in the database compartment."
       is_enabled          = var.create_events_as_enabled
-      condition           = <<EOT
-            {"eventType": 
-            [${local.database_events}]
-            }
-            EOT
+      condition           = jsonencode({"eventType": local.database_events})
       actions_action_type = "ONS"
       actions_is_enabled  = true
       actions_description = "Sends notification via ONS"
       topic_id            = local.database_topic.id == null ? module.lz_topics.topics[local.database_topic.key].id : local.database_topic.id
       defined_tags        = local.notifications_defined_tags
       freeform_tags       = local.notifications_freeform_tags
-    } if length(var.database_admin_email_endpoints)  > 0},
+    } if length(var.database_admin_email_endpoints) > 0},
 
      
      {for i in [1] : (local.notify_on_exainfra_changes_rule.key) => {     

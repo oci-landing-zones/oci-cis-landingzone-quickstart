0ee48f2b79ee4a891b015f5575b2905dd4ebe555
Andre Correa <andre.correa@oracle.com>
2023-01-13 14:31:41 -0300

    format: identation

--- modules/monitoring/logs/main.tf
+++ modules/monitoring/logs/main.tf
@@ -10,31 +10,29 @@ terraform {
 }
 
 resource "oci_logging_log_group" "this" {
-    compartment_id = var.compartment_id
-    display_name   = var.log_group_display_name
-    description    = var.log_group_description
-    defined_tags   = var.defined_tags
-    freeform_tags  = var.freeform_tags
+  compartment_id = var.compartment_id
+  display_name   = var.log_group_display_name
+  description    = var.log_group_description
+  defined_tags   = var.defined_tags
+  freeform_tags  = var.freeform_tags
 }
 
 resource "oci_logging_log" "these" {
-    for_each = var.target_resources 
-        display_name = each.value.log_display_name
-        log_group_id = oci_logging_log_group.this.id
-        log_type     = each.value.log_type
-    
-        configuration {
-            source {
-                category    = each.value.log_config_source_category
-                resource    = each.value.log_config_source_resource
-                service     = each.value.log_config_source_service
-                source_type = each.value.log_config_source_source_type
-            }
-            compartment_id = each.value.log_config_compartment
-        }
-
-        is_enabled         = each.value.log_is_enabled
-        retention_duration = each.value.log_retention_duration
-        defined_tags       = each.value.defined_tags
-        freeform_tags      = each.value.freeform_tags
+  for_each = var.target_resources 
+    display_name = each.value.log_display_name
+    log_group_id = oci_logging_log_group.this.id
+    log_type     = each.value.log_type
+    configuration {
+      source {
+        category    = each.value.log_config_source_category
+        resource    = each.value.log_config_source_resource
+        service     = each.value.log_config_source_service
+        source_type = each.value.log_config_source_source_type
+      }
+      compartment_id = each.value.log_config_compartment
+    }
+    is_enabled         = each.value.log_is_enabled
+    retention_duration = each.value.log_retention_duration
+    defined_tags       = each.value.defined_tags
+    freeform_tags      = each.value.freeform_tags
 }

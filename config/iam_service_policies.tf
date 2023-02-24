d854f88b5599c8d34b99549e56047c5630e03f77
Andre Correa <andre.correa@oracle.com>
2023-02-24 13:33:34 -0300

    refact: module for managing service policies

--- config/iam_service_policies.tf
+++ config/iam_service_policies.tf
@@ -1,46 +1,53 @@
-# Copyright (c) 2021 Oracle and/or its affiliates.
+# Copyright (c) 2023 Oracle and/or its affiliates.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 locals {
-  all_service_policy_statements = []
-
-  all_service_policy_defined_tags = {}
-  all_service_policy_freeform_tags = {}
-
-  # Names
-  services_policy_name   = "${var.service_label}-services-policy"
-
-  cloud_guard_statements = [
-    "Allow service cloudguard to read all-resources in tenancy",
-    "Allow service cloudguard to use network-security-groups in tenancy"
-  ]
-
-  os_mgmt_statements = [
-    "Allow service osms to read instances in tenancy"
-  ]
-
-  default_service_policy_defined_tags = null
-  default_service_policy_freeform_tags = local.landing_zone_tags
-
-  service_policy_defined_tags = length(local.all_service_policy_defined_tags) > 0 ? local.all_service_policy_defined_tags : local.default_service_policy_defined_tags
-  service_policy_freeform_tags = length(local.all_service_policy_freeform_tags) > 0 ? merge(local.all_service_policy_freeform_tags, local.default_service_policy_freeform_tags) : local.default_service_policy_freeform_tags
-
-  default_service_policy_statements = concat(local.cloud_guard_statements, local.os_mgmt_statements)
-
-  service_policies = {
-    (local.services_policy_name) = {
-      compartment_id = var.tenancy_ocid
-      description    = "Landing Zone policy for OCI services: Cloud Guard, Vulnerability Scanning and OS Management."
-      statements     = length(local.all_service_policy_statements) > 0 ? local.all_service_policy_statements : local.default_service_policy_statements
-      defined_tags = local.service_policy_defined_tags
-      freeform_tags = local.service_policy_freeform_tags
-    }
-  }
+#--------------------------------------------------------------------------
+#-- Any of these custom variables can be overriden in a _override.tf file.
+#--------------------------------------------------------------------------  
+  custom_service_policy_defined_tags = null
+  custom_service_policy_freeform_tags = null
 }
 
 module "lz_services_policy" {
-  depends_on = [module.lz_dynamic_groups]
-  source = "../modules/iam/iam-policy"
-  providers = { oci = oci.home }
-  policies   = var.extend_landing_zone_to_new_region == false ? (local.use_existing_root_cmp_grants == true ? {} : local.service_policies) : {}
+  source                        = "../modules/iam/iam-services-policy"
+  providers                     = { oci = oci.home }
+  tenancy_id                    = var.tenancy_ocid
+  enable_tenancy_level_policies = var.extend_landing_zone_to_new_region == false ? (local.use_existing_root_cmp_grants == true ? false : true) : false
+  tenancy_policy_name           = "${var.service_label}-services-policy"
+  defined_tags                  = local.service_policy_defined_tags
+  freeform_tags                 = local.service_policy_freeform_tags
+  policies                      = local.service_policies
 }
+
+locals {
+#--------------------------------------------------------------------------
+#-- These variables are NOT meant to be overriden.
+#--------------------------------------------------------------------------  
+  default_service_policy_defined_tags = null
+  default_service_policy_freeform_tags = local.landing_zone_tags
+
+  service_policy_defined_tags = local.custom_service_policy_defined_tags != null ? merge(local.custom_service_policy_defined_tags, local.default_service_policy_defined_tags) : local.default_service_policy_defined_tags
+  service_policy_freeform_tags = local.custom_service_policy_freeform_tags != null ? merge(local.custom_service_policy_freeform_tags, local.default_service_policy_freeform_tags) : local.default_service_policy_freeform_tags
+
+  realm = split(".",trimprefix(data.oci_identity_tenancy.this.id, "ocid1.tenancy."))[0]
+
+  service_policies = {
+    ("VAULT-GLOBAL-POLICY") = {
+      name           = "${var.service_label}-vault-policy"
+      compartment_id = local.enclosing_compartment_id
+      description    = "Landing Zone policy for OCI services: Blockstorage, OKE and streams to use keys in the ${local.security_compartment.name} compartment."
+      statements     = ["Allow service blockstorage, oke, streaming, Fss${local.realm}Prod to use keys in compartment ${local.security_compartment.name}"]
+      defined_tags   = local.service_policy_defined_tags
+      freeform_tags  = local.service_policy_freeform_tags
+    },
+    ("VAULT-REGIONAL-POLICY") = {
+      name           = "${var.service_label}-vault-${var.region}-policy"
+      compartment_id = local.enclosing_compartment_id
+      description    = "Landing Zone policy for OCI services: Object Storage in ${var.region} to use keys in the ${local.security_compartment.name} compartment."
+      statements     = ["Allow service objectstorage-${var.region} to use keys in compartment ${local.security_compartment.name}"]
+      defined_tags   = local.service_policy_defined_tags
+      freeform_tags  = local.service_policy_freeform_tags
+    }
+  }
+}  

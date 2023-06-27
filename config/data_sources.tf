0809f7deb05974fa2478a1d910a511ebb99f0a4c
Andre Correa <andre.correa@oracle.com>
2023-06-27 15:25:16 -0300

    feat: services policy statements consolidated into single policy

--- config/data_sources.tf
+++ config/data_sources.tf
@@ -3,6 +3,10 @@
 
 data "oci_identity_regions" "these" {}
 
+data "oci_identity_region_subscriptions" "these" {
+  tenancy_id = var.tenancy_ocid
+}
+
 data "oci_identity_tenancy" "this" {
   tenancy_id = var.tenancy_ocid
 }

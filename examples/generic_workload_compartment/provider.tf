157125c1ad05ec441c2a72a4bd8dc2ffd3260d77
josh_hammer <josh.hammer@oracle.com>
2023-08-07 14:24:32 +0000

    fix: Updated terraform version

--- examples/generic_workload_compartment/provider.tf
+++ examples/generic_workload_compartment/provider.tf
@@ -21,8 +21,8 @@ provider "oci" {
 }
 
  terraform {
-  required_version = ">= 1.1.0"
-
+  required_version = ">= 1.2.0, < 1.3.0"
+  
   required_providers {
     oci = {
       source                = "oracle/oci"

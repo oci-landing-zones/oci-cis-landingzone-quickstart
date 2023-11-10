596d35b54aeaee5b03441c18ce814f51822875cd
Andre Correa <andre.correa@oracle.com>
2023-11-10 11:16:50 -0300

    feat: ORM application information tab support

--- pre-config/outputs.tf
+++ pre-config/outputs.tf
@@ -10,3 +10,15 @@ output "lz_provisioning_groups" {
 output "lz_groups" {
   value = { for k in keys(local.enclosing_compartments) : k => module.lz_groups[k].groups if length(module.lz_groups) > 0 }
 }
+output "lz_dynamic_groups" {
+  value = { for k in keys(local.enclosing_compartments) : k => module.lz_dynamic_groups[k].dynamic_groups if length(module.lz_dynamic_groups) > 0 }
+}
+output "home_region" {
+    value = var.home_region
+}
+output "release" {
+    value = fileexists("${path.module}/../release.txt") ? file("${path.module}/../release.txt") : "undefined"
+}
+output "unique_prefix" {
+    value = var.unique_prefix
+}

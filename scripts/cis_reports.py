f827054d08f071cf59dbc373670411f1844d22af
Josh Hammer <josh.hammer@oracle.com>
2026-02-11 19:12:17 -0500

    Chore:  updated date and release

--- scripts/cis_reports.py
+++ scripts/cis_reports.py
@@ -41,9 +41,9 @@ try:
 except Exception:
     OUTPUT_DIAGRAMS = False
 
-RELEASE_VERSION = "3.1.2"
+RELEASE_VERSION = "3.2.0"
 PYTHON_SDK_VERSION = "2.165.1"
-UPDATED_DATE = "January 22, 2025"
+UPDATED_DATE = "March XX, 2026"
 
 
 ##########################################################################

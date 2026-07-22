4670e910709b9137b0f5126c83b893f438230bdf
Josh Hammer <josh.hammer@oracle.com>
2026-07-22 11:32:16 -0400

    chore: Updated copyright date to 2026

--- terraform-remediations/cis_oci_function/logging.tf
+++ terraform-remediations/cis_oci_function/logging.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 resource "oci_logging_log_group" "this" {
--- terraform-remediations/cis_oci_function/resource_scheduler.tf
+++ terraform-remediations/cis_oci_function/resource_scheduler.tf
@@ -1,4 +1,4 @@
-# Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
 resource "oci_resource_scheduler_schedule" "cis_reports" {

5119f57cdac256e1d00961960fb5c5137b5275ea
Halimer <josh.hammer@oracle.com>
2026-07-24 15:35:41 -0400

    chore: added copyright

--- terraform-remediations/cis_oci_function/src/cis-reports/func.py
+++ terraform-remediations/cis_oci_function/src/cis-reports/func.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
 import io
 import hashlib
 import importlib.util
